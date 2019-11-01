4d504bce7e81f3823831bf163a013f92c174187a
Somogyi Márton <kamarton@gmail.com>
2019-11-01 10:19:08 +0100

    Fix #31: Adding successful ResultSet overwrites errors (#33)

---
+++ tests/ResultTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Yiisoft\Validator\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Validator\Result;
+use Yiisoft\Validator\ResultSet;
+
+class ResultTest extends TestCase
+{
+
+    public function testSetErrorAndSuccess(): void {
+        $resultSet = new ResultSet();
+        $result = new Result();
+        $result->addError('test');
+        $resultSet->addResult('x', $result);
+        $resultSet->addResult('x', new Result());
+        $this->assertFalse($resultSet->getResult('x')->isValid());
+    }
+}
