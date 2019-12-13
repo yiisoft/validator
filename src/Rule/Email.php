a2584fb346a6ea3b277ef99b192b7ffe1ca5405d
thenotsoft <44147615+thenotsoft@users.noreply.github.com>
2019-12-13 22:45:27 +0200

    Early return and combining these two checks

--- src/Rule/Email.php
+++ src/Rule/Email.php
@@ -61,34 +61,34 @@ class Email extends Rule
                 $value,
                 $matches
             )) {
-            $valid = false;
-        } else {
-            if ($this->enableIDN) {
-                $matches['local'] = $this->idnToAscii($matches['local']);
-                $matches['domain'] = $this->idnToAscii($matches['domain']);
-                $value = $matches['name'] . $matches['open'] . $matches['local'] . '@' . $matches['domain'] . $matches['close'];
-            }
-
-            if (strlen($matches['local']) > 64) {
-                // The maximum total length of a user name or other local-part is 64 octets. RFC 5322 section 4.5.3.1.1
-                // http://tools.ietf.org/html/rfc5321#section-4.5.3.1.1
-                $valid = false;
-            } elseif (strlen($matches['local'] . '@' . $matches['domain']) > 254) {
-                // There is a restriction in RFC 2821 on the length of an address in MAIL and RCPT commands
-                // of 254 characters. Since addresses that do not fit in those fields are not normally useful, the
-                // upper limit on address lengths should normally be considered to be 254.
-                //
-                // Dominic Sayers, RFC 3696 erratum 1690
-                // http://www.rfc-editor.org/errata_search.php?eid=1690
-                $valid = false;
-            } else {
-                $valid = preg_match($this->pattern, $value) || $this->allowName && preg_match($this->fullPattern, $value);
-                if ($valid && $this->checkDNS) {
-                    $valid = checkdnsrr($matches['domain'] . '.', 'MX') || checkdnsrr($matches['domain'] . '.', 'A');
-                }
-            }
+            $result->addError($this->message);
+            return $result;
         }
 
+        if ($this->enableIDN) {
+            $matches['local'] = $this->idnToAscii($matches['local']);
+            $matches['domain'] = $this->idnToAscii($matches['domain']);
+            $value = $matches['name'] . $matches['open'] . $matches['local'] . '@' . $matches['domain'] . $matches['close'];
+        }
+
+        // The maximum total length of a user name or other local-part is 64 octets. RFC 5322 section 4.5.3.1.1
+        // http://tools.ietf.org/html/rfc5321#section-4.5.3.1.1
+
+        // There is a restriction in RFC 2821 on the length of an address in MAIL and RCPT commands
+        // of 254 characters. Since addresses that do not fit in those fields are not normally useful, the
+        // upper limit on address lengths should normally be considered to be 254.
+        //
+        // Dominic Sayers, RFC 3696 erratum 1690
+        // http://www.rfc-editor.org/errata_search.php?eid=1690
+        if (strlen($matches['local']) > 64 || strlen($matches['local'] . '@' . $matches['domain']) > 254) {
+            $result->addError($this->message);
+            return $result;
+        }
+
+        $valid = preg_match($this->pattern, $value) || $this->allowName && preg_match($this->fullPattern, $value);
+        if ($valid && $this->checkDNS) {
+            $valid = checkdnsrr($matches['domain'] . '.', 'MX') || checkdnsrr($matches['domain'] . '.', 'A');
+        }
 
         if ($valid === false) {
             $result->addError($this->message);
