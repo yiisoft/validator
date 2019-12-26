1d60477c1e29649a5024b663ddc1444c2b649ecb
Alexander Makarov <sam@rmcreative.ru>
2019-12-26 14:40:49 +0300

    Implement Each validator

--- src/Rule/Each.php
+++ src/Rule/Each.php
@@ -1,185 +1,43 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
-
 namespace Yiisoft\Validator\Rule;
 
-use yii\helpers\Yii;
-use yii\exceptions\InvalidConfigException;
-use yii\base\Model;
 use Yiisoft\Validator\DataSetInterface;
 use Yiisoft\Validator\Result;
 use Yiisoft\Validator\Rule;
+use Yiisoft\Validator\Rules;
 
 /**
- * EachValidator validates an array by checking each of its elements against an embedded validation rule.
- *
- * ```php
- * class MyModel extends Model
- * {
- *     public $categoryIDs = [];
- *
- *     public function rules()
- *     {
- *         return [
- *             // checks if every category ID is an integer
- *             ['categoryIDs', 'each', 'rule' => ['integer']],
- *         ]
- *     }
- * }
- * ```
- *
- * > Note: This validator will not work with inline validation rules in case of usage outside the model scope,
- *   e.g. via [[validateValue()]] method.
- *
- * > Note: EachValidator is meant to be used only in basic cases, you should consider usage of tabular input,
- *   using several models for the more complex case.
+ * Each validator validates an array by checking each of its elements against a set of rules
  */
 class Each extends Rule
 {
-    /**
-     * @var array|Rule definition of the validation rule, which should be used on array values.
-     * It should be specified in the same format as at [[\yii\base\Model::rules()]], except it should not
-     * contain attribute list as the first element.
-     * For example:
-     *
-     * ```php
-     * ['integer']
-     * ['match', 'pattern' => '/[a-z]/is']
-     * ```
-     *
-     * Please refer to [[\yii\base\Model::rules()]] for more details.
-     */
-    private $rule;
-    /**
-     * @var bool whether to use error message composed by validator declared via [[rule]] if its validation fails.
-     * If enabled, error message specified for this validator itself will appear only if attribute value is not an array.
-     * If disabled, own error message value will be used always.
-     */
-    private $allowMessageFromRule = true;
-    /**
-     * @var bool whether to stop validation once first error among attribute value elements is detected.
-     * When enabled validation will produce single error message on attribute, when disabled - multiple
-     * error messages mya appear: one per each invalid value.
-     * Note that this option will affect only [[validateAttribute()]] value, while [[validateValue()]] will
-     * not be affected.
-     */
-    private $stopOnFirstError = true;
+    private Rules $rules;
 
-    private $message;
+    private string $incorrectInputMessage = 'Value should be array or iterable';
+    private string $message = '{error} {value} given.';
 
-    /**
-     * @var Rule validator instance.
-     */
-    private $_validator;
-
-    public function __construct()
+    public function __construct(Rules $rules)
     {
-        $this->message = '{attribute} is invalid.';
-    }
-
-    /**
-     * Returns the validator declared in [[rule]].
-     * @param DataSetInterface|null $data model in which context validator should be created.
-     * @return Rule the declared validator.
-     */
-    private function getValidator(DataSetInterface $data = null)
-    {
-        if ($this->_validator === null) {
-            $this->_validator = $this->createEmbeddedValidator($data);
-        }
-
-        return $this->_validator;
-    }
-
-    /**
-     * Creates validator object based on the validation rule specified in [[rule]].
-     * @param DataSetInterface|null $data model in which context validator should be created.
-     * @return Rule validator instance
-     *@throws \yii\exceptions\InvalidConfigException
-     */
-    private function createEmbeddedValidator(DataSetInterface $data)
-    {
-        $rule = $this->rule;
-        if ($rule instanceof Rule) {
-            return $rule;
-        }
-
-        if (is_array($rule) && isset($rule[0])) { // validator type
-            if (!is_object($data)) {
-                $data = new Model(); // mock up context model
-            }
-
-            return Rule::createValidator($rule[0], $data, $this->attributes, array_slice($rule, 1));
-        }
-
-        throw new InvalidConfigException('Invalid validation rule: a rule must be an array specifying validator type.');
-    }
-
-    public function validateAttribute(DataSetInterface $data, string $attribute): Result
-    {
-        $result = new Result();
-        $value = $data->getValue($attribute);
-        if (!is_array($value) && !$value instanceof \ArrayAccess) {
-            return $result->addError($this->formatMessage($this->message));
-        }
-
-        $validator = $this->getValidator($data); // ensure model context while validator creation
-
-        $detectedErrors = $result->getErrors();
-
-        foreach ($value as $k => $v) {
-            $model->clearErrors($attribute);
-            $model->$attribute = $v;
-            if (!$validator->skipOnEmpty || !$validator->isEmpty($v)) {
-                $validator->validateAttribute($model, $attribute);
-            }
-            $filteredValue[$k] = $model->$attribute;
-            if ($model->hasErrors($attribute)) {
-                if ($this->allowMessageFromRule) {
-                    $validationErrors = $model->getErrors($attribute);
-                    $detectedErrors = array_merge($detectedErrors, $validationErrors);
-                } else {
-                    $model->clearErrors($attribute);
-                    $result = $result->addError($this->formatMessage($this->message, ['value' => $v]));
-                    $detectedErrors[] = $model->getFirstError($attribute);
-                }
-                $model->$attribute = $value;
-
-                if ($this->stopOnFirstError) {
-                    break;
-                }
-            }
-        }
-
-
-        $model->clearErrors($attribute);
-        $model->addErrors([$attribute => $detectedErrors]);
+        $this->rules = $rules;
     }
 
     protected function validateValue($value, DataSetInterface $dataSet = null): Result
     {
         $result = new Result();
-        if (!is_array($value) && !$value instanceof \ArrayAccess) {
-            $result->addError($this->formatMessage($this->message));
+        if (!is_iterable($value)) {
+            return $result->addError($this->incorrectInputMessage);
         }
 
-        $validator = $this->getValidator();
-        foreach ($value as $v) {
-            if ($validator->shouldSkipOnEmpty() && $validator->isEmpty($v)) {
-                continue;
-            }
-            $result = $validator->validateValue($v);
-            if ($result !== null) {
-                if ($this->allowMessageFromRule) {
-                    $result[1]['value'] = $v;
-                    return $result;
+        foreach ($value as $item) {
+            $itemResult = $this->rules->validate($item, $dataSet);
+            if ($itemResult->isValid() === false) {
+                foreach ($itemResult->getErrors() as $error) {
+                    $message = $this->formatMessage($this->message, [
+                        'error' => $error,
+                        'value' => $item,
+                    ]);
+                    $result = $result->addError($message);
                 }
-
-                return [$this->message, ['value' => $v]];
             }
         }
 
--- tests/Rule/EachTest.php
+++ tests/Rule/EachTest.php
@@ -3,183 +3,35 @@ namespace Yiisoft\Validator\Tests\Rule;
 
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Validator\Rule\Each;
+use Yiisoft\Validator\Rule\Number;
+use Yiisoft\Validator\Rules;
 
 /**
  * @group validators
  */
 class EachTest extends TestCase
 {
-    public function testArrayFormat()
-    {
-        $validator = new Each(['rule' => ['required']]);
-
-        $this->assertFalse($validator->validate('not array'));
-        $this->assertTrue($validator->validate(['value']));
-    }
-
     /**
-     * @depends testArrayFormat
+     * @test
      */
-    public function testValidate()
+    public function validateValues(): void
     {
-        $validator = new Each(['rule' => ['integer']]);
+        $values = [
+            10, 20, 30
+        ];
 
-        $this->assertTrue($validator->validate([1, 3, 8]));
-        $this->assertFalse($validator->validate([1, 'text', 8]));
-    }
-
-    /**
-     * @depends testArrayFormat
-     */
-    public function testFilter()
-    {
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_one' => [
-                '  to be trimmed  ',
-            ],
-        ]);
-        $validator = new Each(['rule' => ['trim']]);
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertEquals('to be trimmed', $model->attr_one[0]);
-    }
-
-    /**
-     * @depends testValidate
-     */
-    public function testAllowMessageFromRule()
-    {
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_one' => [
-                'text',
-            ],
-        ]);
-        $validator = new Each(['rule' => ['integer']]);
-
-        $validator->allowMessageFromRule = true;
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertContains('integer', $model->getFirstError('attr_one'));
-
-        $model->clearErrors();
-        $validator->allowMessageFromRule = false;
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertNotContains('integer', $model->getFirstError('attr_one'));
-    }
-
-    /**
-     * @depends testValidate
-     */
-    public function testCustomMessageValue()
-    {
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_one' => [
-                'TEXT',
-            ],
-        ]);
-        $validator = new Each(['rule' => ['integer', 'message' => '{value} is not an integer']]);
-
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertSame('TEXT is not an integer', $model->getFirstError('attr_one'));
-
-        $model->clearErrors();
-        $validator->allowMessageFromRule = false;
-        $validator->message = '{value} is invalid';
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertEquals('TEXT is invalid', $model->getFirstError('attr_one'));
-    }
-
-    /**
-     * @see https://github.com/yiisoft/yii2/issues/10825
-     *
-     * @depends testValidate
-     */
-    public function testSkipOnEmpty()
-    {
-        $validator = new Each(['rule' => ['integer', 'skipOnEmpty' => true]]);
-        $this->assertTrue($validator->validate(['']));
-
-        $validator = new Each(['rule' => ['integer', 'skipOnEmpty' => false]]);
-        $this->assertFalse($validator->validate(['']));
-
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_one' => [
-                '',
-            ],
-        ]);
-        $validator = new Each(['rule' => ['integer', 'skipOnEmpty' => true]]);
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertFalse($model->hasErrors('attr_one'));
-
-        $model->clearErrors();
-        $validator = new Each(['rule' => ['integer', 'skipOnEmpty' => false]]);
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertTrue($model->hasErrors('attr_one'));
-    }
-
-    /**
-     * @see https://github.com/yiisoft/yii2/issues/9935
-     *
-     * @depends testValidate
-     */
-    public function testCompare()
-    {
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_one' => [
-                'value1',
-                'value2',
-                'value3',
-            ],
-            'attr_two' => 'value2',
-        ]);
-        $validator = new Each(['rule' => ['compare', 'compareAttribute' => 'attr_two']]);
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertNotEmpty($model->getErrors('attr_one'));
-        $this->assertCount(3, $model->attr_one);
-
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_one' => [
-                'value1',
-                'value2',
-                'value3',
-            ],
-            'attr_two' => 'value4',
-        ]);
-        $validator = new Each(['rule' => ['compare', 'compareAttribute' => 'attr_two', 'operator' => '!=']]);
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertEmpty($model->getErrors('attr_one'));
-    }
-
-    /**
-     * @depends testValidate
-     */
-    public function testStopOnFirstError()
-    {
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_one' => [
-                'one', 2, 'three',
-            ],
-        ]);
-        $validator = new Each(['rule' => ['integer']]);
-
-        $validator->stopOnFirstError = true;
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertCount(1, $model->getErrors('attr_one'));
-
-        $model->clearErrors();
-        $validator->stopOnFirstError = false;
-        $validator->validateAttribute($model, 'attr_one');
-        $this->assertCount(2, $model->getErrors('attr_one'));
-    }
-
-    public function testValidateArrayAccess()
-    {
-        $model = FakedValidationModel::createWithAttributes([
-            'attr_array' => new ArrayAccessObject([1,2,3]),
+        $rules = new Rules([
+            (new Number())->max(13)
         ]);
 
-        $validator = new Each(['rule' => ['integer']]);
-        $validator->validateAttribute($model, 'attr_array');
-        $this->assertFalse($model->hasErrors('array'));
+        $eachRule = new Each($rules);
 
-        $this->assertTrue($validator->validate($model->attr_array));
+        $result = $eachRule->validate($values);
+        $errors = $result->getErrors();
+
+        $this->assertFalse($result->isValid());
+        $this->assertCount(2, $errors);
+        $this->assertContains('Value must be no greater than 13. 20 given.', $errors);
+        $this->assertContains('Value must be no greater than 13. 30 given.', $errors);
     }
 }
