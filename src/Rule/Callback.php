8aca813614676ac8255618270bfc8597170ee7ce
thenotsoft <44147615+thenotsoft@users.noreply.github.com>
2020-03-02 14:16:29 +0200

    Allow using private methods in Callback rule (#58)

--- src/Rule/Callback.php
+++ src/Rule/Callback.php
@@ -8,12 +8,22 @@ use Yiisoft\Validator\Rule;
 use Yiisoft\Validator\Result;
 use Yiisoft\Validator\DataSetInterface;
 
+/**
+ * Callback rule could be used to create custom rules without defining a class.
+ * If callback is specified as [className, methodName], the method of the class could be private.
+ */
 class Callback extends Rule
 {
     private $callback;
 
-    public function __construct(callable $callback)
+    public function __construct($callback)
     {
+        if (!(is_callable($callback) || (is_array($callback) && count($callback) === 2))) {
+            throw new \InvalidArgumentException(
+                'The argument must be a callable or an array with the class and method name.'
+            );
+        }
+
         $this->callback = $callback;
     }
 
@@ -21,15 +31,46 @@ class Callback extends Rule
     {
         $result = new Result();
         $callback = $this->callback;
+
         /**
          * @var $callbackResult Result
          */
-        $callbackResult = $callback($value, $dataSet);
+        $callbackResult = is_callable($callback)
+            ? $callback($value, $dataSet)
+            : $this->invokeMethod($value, $callback);
+
         if ($callbackResult->isValid() === false) {
             foreach ($callbackResult->getErrors() as $message) {
                 $result->addError($this->translateMessage($message));
             }
         }
         return $result;
+    }
+
+    private function invokeMethod($value, array $callback): Result
+    {
+        [$class, $method] = $callback;
+        if (!method_exists($class, $method)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'Method "%s" targeted by Callback rule does not exist in class %s',
+                    $method,
+                    \get_class($class)
+                )
+            );
+        }
+        $reflectionMethod = new \ReflectionMethod($class, $method);
+        if (is_string($class) && !$reflectionMethod->isStatic()) {
+            throw new \InvalidArgumentException(
+                sprintf('Method "%s" targeted by Callback rule must be static.', $method)
+            );
+        }
+        if (!$reflectionMethod->isPublic()) {
+            $reflectionMethod->setAccessible(true);
+        }
+        if ($reflectionMethod->isStatic()) {
+            return $reflectionMethod->invoke(null, $value);
+        }
+        return $reflectionMethod->invoke($class, $value);
     }
 }
--- tests/Rule/CallbackTest.php
+++ tests/Rule/CallbackTest.php
@@ -8,8 +8,64 @@ use PHPUnit\Framework\TestCase;
 use Yiisoft\Validator\Result;
 use Yiisoft\Validator\Rule\Callback;
 
+class CallbackTest_Object
+{
+    private function privateMethod($value): Result
+    {
+        $result = new Result();
+        if ($value === 'test') {
+            $result->addError('error message');
+        }
+        return $result;
+    }
+
+    private function protectedMethod($value): Result
+    {
+        $result = new Result();
+        if ($value === 'test') {
+            $result->addError('error message');
+        }
+        return $result;
+    }
+
+    public function publicMethod($value): Result
+    {
+        $result = new Result();
+        if ($value === 'test') {
+            $result->addError('error message');
+        }
+        return $result;
+    }
+
+    public static function staticMethod($value): Result
+    {
+        $result = new Result();
+        if ($value === 'test') {
+            $result->addError('error message');
+        }
+        return $result;
+    }
+
+    private static function privateStaticMethod($value): Result
+    {
+        $result = new Result();
+        if ($value === 'test') {
+            $result->addError('error message');
+        }
+        return $result;
+    }
+}
+
 class CallbackTest extends TestCase
 {
+    public function testThrowExceptionInvalidArgument()
+    {
+        foreach ([null, 'test', []] as $value) {
+            $this->expectException(\InvalidArgumentException::class);
+            new Callback($value);
+        }
+    }
+
     public function testValidate(): void
     {
         $rule = new Callback(
@@ -27,5 +83,74 @@ class CallbackTest extends TestCase
         $this->assertFalse($result->isValid());
         $this->assertCount(1, $result->getErrors());
         $this->assertEquals('Value should be 42!', $result->getErrors()[0]);
+    }
+
+    public function testValidatePrivateMethod()
+    {
+        $rule = new Callback([new CallbackTest_Object(), 'privateMethod']);
+
+        $result = $rule->validate('test');
+        $this->assertFalse($result->isValid());
+        $this->assertCount(1, $result->getErrors());
+    }
+
+    public function testValidateProtectedMethod()
+    {
+        $rule = new Callback([new CallbackTest_Object(), 'protectedMethod']);
+
+        $result = $rule->validate('test');
+        $this->assertFalse($result->isValid());
+        $this->assertCount(1, $result->getErrors());
+    }
+
+    public function testValidatePublicMethod()
+    {
+        $rule = new Callback([new CallbackTest_Object(), 'publicMethod']);
+
+        $result = $rule->validate('test');
+        $this->assertFalse($result->isValid());
+        $this->assertCount(1, $result->getErrors());
+    }
+
+    public function testThrowExceptionIfNoPassedObject()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        (new Callback([CallbackTest_Object::class, 'protectedMethod']))->validate(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        (new Callback([CallbackTest_Object::class, 'privateMethod']))->validate(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        (new Callback([CallbackTest_Object::class, 'publicMethod']))->validate(null);
+    }
+
+    public function testValidatePublicStaticMethod()
+    {
+        $rule = new Callback([new CallbackTest_Object(), 'staticMethod']);
+
+        $result = $rule->validate('test');
+        $this->assertFalse($result->isValid());
+        $this->assertCount(1, $result->getErrors());
+
+        $rule = new Callback([CallbackTest_Object::class, 'staticMethod']);
+
+        $result = $rule->validate('test');
+        $this->assertFalse($result->isValid());
+        $this->assertCount(1, $result->getErrors());
+    }
+
+    public function testValidatePrivateStaticMethod()
+    {
+        $rule = new Callback([new CallbackTest_Object(), 'privateStaticMethod']);
+
+        $result = $rule->validate('test');
+        $this->assertFalse($result->isValid());
+        $this->assertCount(1, $result->getErrors());
+
+        $rule = new Callback([CallbackTest_Object::class, 'privateStaticMethod']);
+
+        $result = $rule->validate('test');
+        $this->assertFalse($result->isValid());
+        $this->assertCount(1, $result->getErrors());
     }
 }
