f73e4dbc2d0a138034982e146bce12bdd5e729c3
Alexander Makarov <sam@rmcreative.ru>
2020-03-03 22:15:17 +0300

    Remove unused import

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator\Tests;
 
-use Yiisoft\Validator\Rule;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Validator\DataSetInterface;
 use Yiisoft\Validator\MissingAttributeException;
