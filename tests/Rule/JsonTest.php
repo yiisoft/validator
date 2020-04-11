05e0ddcf3c2c31e83d9c0fb25939203afb824964
rcurkanu <rcurkanu@stream.ru>
2020-04-11 20:56:11 +0300

    add tests

---
+++ tests/Rule/JsonTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Tests\Rule;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Validator\Rule\Json;
+
+/**
+ * @group validators
+ */
+class JsonTest extends TestCase
+{
+    public function testInvalidJsonValidate(): void
+    {
+        $val = new Json();
+        $this->assertFalse($val->validate('{"name": "tester"')->isValid());
+        $this->assertFalse($val->validate('{"name": tester}')->isValid());
+    }
+
+    public function testInvalidTypeValidate(): void
+    {
+        $val = new Json();
+        $this->assertFalse($val->validate(['json'])->isValid());
+        $this->assertFalse($val->validate(10)->isValid());
+        $this->assertFalse($val->validate(null)->isValid());
+    }
+
+    public function testValidValueValidate(): void
+    {
+        $this->assertTrue((new Json())->validate('{"name": "tester"}')->isValid());
+    }
+}
