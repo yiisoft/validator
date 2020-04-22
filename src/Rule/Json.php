8ea83f819b04beee9b3f17a90e6a58ac0cd120a4
Arman Poghosyan <armpogart@users.noreply.github.com>
2020-04-23 03:51:35 +0400

    Use regexp for Json validation (#69)

--- src/Rule/Json.php
+++ src/Rule/Json.php
@@ -8,7 +8,6 @@ use Yiisoft\Validator\HasValidationMessage;
 use Yiisoft\Validator\Rule;
 use Yiisoft\Validator\Result;
 use Yiisoft\Validator\DataSetInterface;
-use JsonException;
 
 /**
  * JsonValidator validates that the attribute value is a valid json
@@ -32,16 +31,23 @@ class Json extends Rule
 
     private function isValidJson($value): bool
     {
-        if (!is_string($value)) {
-            return false;
-        }
+        // Regular expression is built based on JSON grammar specified at
+        // https://tools.ietf.org/html/rfc8259
+        $regex = <<<'REGEX'
+/
+(?(DEFINE)
+    (?<json>(?>\s*(?&object)\s*|\s*(?&array)\s*))
+    (?<object>(?>\{\s*(?>(?&member)(?>\s*,\s*(?&member))*)?\s*\}))
+    (?<member>(?>(?&string)\s*:\s*(?&value)))
+    (?<array>(?>\[\s*(?>(?&value)(?>\s*,\s*(?&value))*)?\s*\]))
+    (?<value>(?>)false|null|true|(?&object)|(?&array)|(?&number)|(?&string))
+    (?<number>(?>-?(?>0|[1-9]\d*)(?>\.\d+)?(?>[eE][-+]?\d+)?))
+    (?<string>(?>"(?>\\(?>["\\\/bfnrt]|u[a-fA-F0-9]{4})|[^"\\\0-\x1F\x7F]+)*"))
+)
+\A(?&json)\z
+/x
+REGEX;
 
-        try {
-            json_decode($value, false, 512, JSON_THROW_ON_ERROR);
-        } catch (JsonException $e) {
-            return false;
-        }
-
-        return true;
+        return is_string($value) && preg_match($regex, $value) === 1;
     }
 }
