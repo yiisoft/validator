e9fa9a775cfdf4a5731c1fdbc31f8bb5765bf715
romkatsu <r@tsurkanu.ru>
2020-08-17 19:49:07 +0300

    add tests

--- tests/Rule/UrlTest.php
+++ tests/Rule/UrlTest.php
@@ -10,15 +10,19 @@ use Yiisoft\Validator\Rule\Url;
  */
 class UrlTest extends TestCase
 {
-    public function testValidate()
+    public function testValidate(): void
     {
         $val = new Url();
         $this->assertFalse($val->validate('google.de')->isValid());
         $this->assertTrue($val->validate('http://google.de')->isValid());
         $this->assertTrue($val->validate('https://google.de')->isValid());
         $this->assertFalse($val->validate('htp://yiiframework.com')->isValid());
-        $this->assertTrue($val->validate('https://www.google.de/search?q=yii+framework&ie=utf-8&oe=utf-8'
-                                        . '&rls=org.mozilla:de:official&client=firefox-a&gws_rd=cr')->isValid());
+        $this->assertTrue(
+            $val->validate(
+                'https://www.google.de/search?q=yii+framework&ie=utf-8&oe=utf-8'
+                . '&rls=org.mozilla:de:official&client=firefox-a&gws_rd=cr'
+            )->isValid()
+        );
         $this->assertFalse($val->validate('ftp://ftp.ruhr-uni-bochum.de/')->isValid());
         $this->assertFalse($val->validate('http://invalid,domain')->isValid());
         $this->assertFalse($val->validate('http://example.com,')->isValid());
@@ -41,7 +45,7 @@ class UrlTest extends TestCase
         $this->assertFalse($val->validate('http://äüö?=!"§$%&/()=}][{³²€.edu')->isValid());
     }
 
-    public function testValidateWithoutScheme()
+    public function testValidateWithoutScheme(): void
     {
         $val = (new Url())
             ->pattern('/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)/i');
@@ -49,7 +53,7 @@ class UrlTest extends TestCase
         $this->assertTrue($val->validate('yiiframework.com')->isValid());
     }
 
-    public function testValidateWithCustomScheme()
+    public function testValidateWithCustomScheme(): void
     {
         $val = (new Url())
             ->schemes(['http', 'https', 'ftp', 'ftps']);
@@ -62,25 +66,35 @@ class UrlTest extends TestCase
         $this->assertFalse($val->validate('//yiiframework.com')->isValid());
     }
 
-    public function testValidateWithIdn()
+    public function testValidateWithIdn(): void
     {
         if (!function_exists('idn_to_ascii')) {
             $this->markTestSkipped('intl package required');
-
             return;
         }
-        $val = (new Url())
-            ->enableIDN();
 
+        $val = (new Url())->enableIDN();
         $this->assertTrue($val->validate('http://äüößìà.de')->isValid());
         // converted via http://mct.verisign-grs.com/convertServlet
         $this->assertTrue($val->validate('http://xn--zcack7ayc9a.de')->isValid());
     }
 
-    public function testValidateLength()
+
+    public function testValidateLength(): void
     {
         $url = 'http://' . str_pad('base', 2000, 'url') . '.de';
         $val = new Url();
         $this->assertFalse($val->validate($url)->isValid());
+    }
+
+    public function testValidateWithIdnWithoutScheme(): void
+    {
+        if (!function_exists('idn_to_ascii')) {
+            $this->markTestSkipped('intl package required');
+            return;
+        }
+
+        $validator = (new Url())->pattern('/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)/i')->enableIDN();
+        $this->assertTrue($validator->validate('домен.рф')->isValid());
     }
 }
