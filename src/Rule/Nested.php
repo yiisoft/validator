b6593c65ee6a74bd9d08cd53718c4a001ee353de
Dmitry Derepko <xepozz@list.ru>
2021-01-11 22:56:48 +0300

    Add test cases

--- src/Rule/Nested.php
+++ src/Rule/Nested.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator\Rule;
 
+use InvalidArgumentException;
+use Traversable;
 use Yiisoft\Arrays\ArrayHelper;
 use Yiisoft\Validator\DataSetInterface;
 use Yiisoft\Validator\HasValidationErrorMessage;
@@ -23,37 +25,58 @@ class Nested extends Rule
      */
     private iterable $rules;
 
-    private string $incorrectInputMessage = 'Value should be array or iterable';
-    private string $message = '{error} {value} given.';
+    private bool $errorWhenPropertyPathIsNotFound = false;
+    private string $propertyPathIsNotFoundMessage = 'Property path "{path}" is not found.';
+
+    private string $message = 'Nested validation error.';
 
     public function __construct(iterable $rules)
     {
+        $rules = $rules instanceof Traversable ? iterator_to_array($rules) : (array)$rules;
+        if (empty($rules)) {
+            throw new InvalidArgumentException('Rules should not be empty.');
+        }
+        if ($this->checkRules($rules)) {
+            throw new InvalidArgumentException(sprintf(
+                'Each rule should be instance of %s.',
+                Rule::class
+            ));
+        }
         $this->rules = $rules;
+
     }
 
     protected function validateValue($value, DataSetInterface $dataSet = null): Result
     {
         $result = new Result();
-        if (!is_iterable($value)) {
-            $result->addError($this->incorrectInputMessage);
+        if (!is_object($value) && !is_array($value)) {
+            $result->addError(sprintf(
+                'Value should be an array or an object. %s given',
+                gettype($value)
+            ));
             return $result;
         }
+        $value = (array) $value;
 
         foreach ($this->rules as $valuePath => $rules) {
+            $rulesSet = is_array($rules) ? $rules : [$rules];
+            if ($this->errorWhenPropertyPathIsNotFound && !ArrayHelper::pathExists($value, $valuePath)) {
+                $result->addError(
+                    $this->translateMessage(
+                        $this->propertyPathIsNotFoundMessage,
+                        [
+                            'path' => $valuePath,
+                        ]
+                    )
+                );
+                continue;
+            }
             $validatedValue = ArrayHelper::getValueByPath($value, $valuePath);
-            $aggregateRule = new Rules($rules);
+            $aggregateRule = new Rules($rulesSet);
             $itemResult = $aggregateRule->validate($validatedValue);
             if ($itemResult->isValid() === false) {
                 foreach ($itemResult->getErrors() as $error) {
-                    $result->addError(
-                        $this->translateMessage(
-                            $this->message,
-                            [
-                                'error' => $error,
-                                'value' => $validatedValue,
-                            ]
-                        )
-                    );
+                    $result->addError($error);
                 }
             }
         }
@@ -61,15 +84,30 @@ class Nested extends Rule
         return $result;
     }
 
-    public function incorrectInputMessage(string $message): self
+    public function errorWhenPropertyPathIsNotFound(bool $value): self
     {
         $new = clone $this;
-        $new->incorrectInputMessage = $message;
+        $new->errorWhenPropertyPathIsNotFound = $value;
+        return $new;
+    }
+
+    public function propertyPathIsNotFoundMessage(string $message): self
+    {
+        $new = clone $this;
+        $new->propertyPathIsNotFoundMessage = $message;
         return $new;
     }
 
     public function getOptions(): array
     {
         return $this->rules->asArray();
+    }
+
+    private function checkRules(array $rules): bool
+    {
+        return array_reduce(
+            $rules,
+            fn(bool $carry, $rule) => $carry || is_array($rule) ? $this->checkRules($rule) : !$rule instanceof Rule, false
+        );
     }
 }
--- tests/Rule/NestedTest.php
+++ tests/Rule/NestedTest.php
@@ -10,6 +10,7 @@ use Yiisoft\Validator\Rule\HasLength;
 use Yiisoft\Validator\Rule\InRange;
 use Yiisoft\Validator\Rule\Nested;
 use Yiisoft\Validator\Rule\Number;
+use Yiisoft\Validator\Rule\Required;
 
 /**
  * @group validators
@@ -33,6 +34,75 @@ class NestedTest extends TestCase
         $actualResult = (new Nested($rules))->validate($values);
 
         $this->assertEquals($expectedResult, $actualResult->isValid());
+    }
+
+    public function testValidationEmptyRules(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Nested([]);
+    }
+
+    public function testValidationRuleIsNotInstanceOfRule(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Nested(['path.to.value' => (new \stdClass())]);
+    }
+
+    public function testInvalidValue(): void
+    {
+        $validator = (new Nested(['value' => (new Required())]));
+
+        $result = $validator->validate('');
+
+        $this->assertEquals(['Value should be an array or an object. string given'], $result->getErrors());
+    }
+
+    public function testValidationMessage(): void
+    {
+        $validator = (new Nested([
+            'value' => (new Required())->message('Value cannot be blank.'),
+        ]));
+
+        $result = $validator->validate(['value' => null]);
+
+        $this->assertEquals(['Value cannot be blank.'], $result->getErrors());
+    }
+
+    public function testCustomValidationMessage(): void
+    {
+        $validator = (new Nested(['value' => (new Required())]))
+            ->message('Custom message');
+
+        $result = $validator->validate([]);
+
+        $this->assertEquals(['Value cannot be blank.'], $result->getErrors());
+    }
+
+    public function testErrorWhenValuePathNotFound(): void
+    {
+        $validator = (new Nested(['value' => (new Required())]))
+            ->errorWhenPropertyPathIsNotFound(true);
+
+        $result = $validator->validate([]);
+
+        $this->assertEquals(['Property path "value" is not found.'], $result->getErrors());
+    }
+
+    public function testPropertyPathIsNotFoundMessage(): void
+    {
+        $validator = (new Nested(['value' => (new Required())]))
+            ->errorWhenPropertyPathIsNotFound(true)
+            ->propertyPathIsNotFoundMessage('Property is not found.');
+
+        $result = $validator->validate([]);
+
+        $this->assertEquals(['Property is not found.'], $result->getErrors());
+    }
+
+    public function testName(): void
+    {
+        $validator = new Nested(['value' => (new Required())]);
+        $this->assertEquals('nested', $validator->getName());
     }
 
     public function validateDataProvider(): array
