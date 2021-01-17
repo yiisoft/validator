f2fa37b502e4ff6e6accf4f4f9989e75adc3ef88
DarkDef <darkdef@gmail.com>
2021-01-17 14:22:18 +0300

    return factory

---
+++ src/ValidatorFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator;
+
+use Yiisoft\Validator\Rule\Callback;
+
+final class ValidatorFactory implements ValidatorFactoryInterface
+{
+    public function __construct() {
+    }
+
+    public function create(array $rules): ValidatorInterface
+    {
+        return new Validator($this->normalizeRules($rules));
+    }
+
+    private function normalizeRules(array $rules)
+    {
+        foreach ($rules as $attribute => $ruleSets) {
+            foreach ($ruleSets as $index => $rule) {
+                $ruleSets[$index] = $this->normalizeRule($rule);
+            }
+            $rules[$attribute] = $ruleSets;
+        }
+        return $rules;
+    }
+
+    /**
+     * @param callable|Rule $rule
+     */
+    private function normalizeRule($rule): Rule
+    {
+        if (is_callable($rule)) {
+            $rule = new Callback($rule);
+        }
+
+        if (!$rule instanceof Rule) {
+            throw new \InvalidArgumentException(
+                'Rule should be either instance of Rule class or a callable'
+            );
+        }
+
+        return $rule;
+    }
+}
---
+++ tests/ValidatorFactoryTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Validator\DataSetInterface;
+use Yiisoft\Validator\ErrorMessage;
+use Yiisoft\Validator\Result;
+use Yiisoft\Validator\ValidatorFactory;
+
+class ValidatorFactoryTest extends TestCase
+{
+    public function testCreate()
+    {
+        $validation = new ValidatorFactory();
+
+        $attribute = 'test';
+        $errorMessage = 'error message';
+
+        $validator = $validation->create(
+            [
+                $attribute => [
+                    static function () use ($errorMessage) {
+                        $result = new Result();
+                        $result->addError(new ErrorMessage($errorMessage));
+                        return $result;
+                    },
+                ],
+            ]
+        );
+
+        $result = $validator->validate($this->createDataSet([$attribute => '']));
+
+        $this->assertEquals(new ErrorMessage($errorMessage), $result->getResult($attribute)->getErrors()[0]);
+    }
+
+    public function testCreateWithInvalidRule()
+    {
+        $validation = new ValidatorFactory();
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $attribute = 'test';
+        $validation->create(
+            [
+                $attribute => [
+                    'invalid rule',
+                ],
+            ]
+        );
+    }
+
+    private function createDataSet(array $attributes): DataSetInterface
+    {
+        return new class($attributes) implements DataSetInterface {
+            private array $attributes;
+
+            public function __construct(array $attributes)
+            {
+                $this->attributes = $attributes;
+            }
+
+            public function getAttributeValue(string $attribute)
+            {
+                return $this->hasAttribute($attribute) ? $this->attributes[$attribute] : null;
+            }
+
+            public function hasAttribute(string $attribute): bool
+            {
+                return array_key_exists($attribute, $this->attributes);
+            }
+        };
+    }
+}
