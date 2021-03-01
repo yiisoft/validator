98721e57d64149515154c483ab7e2a85fee579e3
kriptonuz <kripton253@gmail.com>
2021-03-01 19:42:36 +0200

    remove subset test from InRangeTest

--- tests/Rule/InRangeTest.php
+++ tests/Rule/InRangeTest.php
@@ -32,22 +32,20 @@ class InRangeTest extends TestCase
         $this->assertFalse($rule->validate('0')->isValid());
         $this->assertFalse($rule->validate(0)->isValid());
         $this->assertFalse($rule->validate('')->isValid());
-
-        $rule = (new InRange(range(10, 20, 1)))
-            ->skipOnEmpty(false);
-
-        $this->assertTrue($rule->validate([])->isValid());
     }
 
     public function testValidateArrayValue(): void
     {
-        $rule = (new InRange(range(1, 10)));
+        // Test in array, values are arrays. IE: ['a'] in [['a'], ['b']]
+        $rule = (new InRange([['a'], ['b']]));
 
-        $this->assertTrue($rule->validate([1, 2, 3, 4, 5])->isValid());
-        $this->assertTrue($rule->validate([6, 7, 8, 9, 10])->isValid());
-        $this->assertFalse($rule->validate([0, 1, 2])->isValid());
-        $this->assertFalse($rule->validate([10, 11, 12])->isValid());
-        $this->assertTrue($rule->validate(['1', '2', '3', 4, 5, 6])->isValid());
+        $this->assertTrue($rule->validate(['a'])->isValid());
+
+
+        // Test range as ArrayObject.
+        $rule = (new InRange(new \ArrayObject(['a', 'b'])));
+
+        $this->assertTrue($rule->validate('a')->isValid());
     }
 
     public function testValidateStrict(): void
@@ -84,30 +82,6 @@ class InRangeTest extends TestCase
         $this->assertFalse($rule->validate(10)->isValid());
         $this->assertFalse($rule->validate('10')->isValid());
         $this->assertFalse($rule->validate('5')->isValid());
-    }
-
-    public function testValidateSubsetArrayable(): void
-    {
-        // Test in array, values are arrays. IE: ['a'] in [['a'], ['b']]
-        $rule = (new InRange([['a'], ['b']]));
-
-        $this->assertTrue($rule->validate(['a'])->isValid());
-
-        // Test in array, values are arrays. IE: ['a', 'b'] subset [['a', 'b', 'c']
-        $rule = (new InRange(['a', 'b', 'c']));
-
-        $this->assertTrue($rule->validate(['a', 'b'])->isValid());
-
-        // Test in array, values are arrays. IE: ['a', 'b'] subset [['a', 'b', 'c']
-        $rule = (new InRange(['a', 'b', 'c']));
-
-        $this->assertTrue($rule->validate(new \ArrayObject(['a', 'b']))->isValid());
-
-
-        // Test range as ArrayObject.
-        $rule = (new InRange(new \ArrayObject(['a', 'b'])));
-
-        $this->assertTrue($rule->validate('a')->isValid());
     }
 
     public function testName(): void
