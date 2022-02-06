54fb6a8b892f18a0f8c92cd4aa1bdf41dca33e0d
Alexey Rogachev <arogachev90@gmail.com>
2022-02-06 13:32:23 +0600

    Fix Psalm error and add array destructuring again

--- src/Error.php
+++ src/Error.php
@@ -7,8 +7,15 @@ namespace Yiisoft\Validator;
 final class Error
 {
     private string $message;
+
+    /**
+     * @psalm-var list<int|string>
+     */
     private array $valuePath;
 
+    /**
+     * @psalm-param list<int|string> $valuePath
+     */
     public function __construct(string $message, array $valuePath = [])
     {
         $this->message = $message;
@@ -21,7 +28,7 @@ final class Error
     }
 
     /**
-     * @psalm-return array<int|string>
+     * @psalm-return list<int|string>
      */
     public function getValuePath(): array
     {
