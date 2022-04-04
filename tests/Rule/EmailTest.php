eeaf94778468277936543461482a3ff1099c9ca3
Alexey Rogachev <arogachev90@gmail.com>
2022-04-04 18:04:11 +0600

    Refactor EmailTest::testValidate()

--- tests/Rule/EmailTest.php
+++ tests/Rule/EmailTest.php
@@ -7,61 +7,88 @@ namespace Yiisoft\Validator\Tests\Rule;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Validator\Rule;
 use Yiisoft\Validator\Rule\Email;
+use function function_exists;
 
 /**
  * @group validators
  */
 class EmailTest extends TestCase
 {
-    public function testValidate(): void
+    public function validateWithDefaultsParametersProvider(): array
+    {
+        return [
+            ['sam@rmcreative.ru', true],
+            ['5011@gmail.com', true],
+            ['Abc.123@example.com', true],
+            ['user+mailbox/department=shipping@example.com', true],
+            ['!#$%&\'*+-/=?^_`.{|}~@example.com', true],
+
+            ['rmcreative.ru', false],
+            ['Carsten Brandt <mail@cebe.cc>', false],
+            ['"Carsten Brandt" <mail@cebe.cc>', false],
+            ['<mail@cebe.cc>', false],
+            ['info@örtliches.de', false],
+            ['sam@рмкреатиф.ru', false],
+            ['ex..ample@example.com', false],
+            [['developer@yiiframework.com'], false],
+        ];
+    }
+
+    /**
+     * @dataProvider validateWithDefaultsParametersProvider
+     */
+    public function testValidateWithDefaultParameters(mixed $value, bool $expectedIsValid): void
     {
         $rule = new Email();
+        $result = $rule->validate($value);
 
-        $this->assertTrue($rule->validate('sam@rmcreative.ru')->isValid());
-        $this->assertTrue($rule->validate('5011@gmail.com')->isValid());
-        $this->assertTrue($rule->validate('Abc.123@example.com')->isValid());
-        $this->assertTrue($rule->validate('user+mailbox/department=shipping@example.com')->isValid());
-        $this->assertTrue($rule->validate('!#$%&\'*+-/=?^_`.{|}~@example.com')->isValid());
-        $this->assertFalse($rule->validate('rmcreative.ru')->isValid());
-        $this->assertFalse($rule->validate('Carsten Brandt <mail@cebe.cc>')->isValid());
-        $this->assertFalse($rule->validate('"Carsten Brandt" <mail@cebe.cc>')->isValid());
-        $this->assertFalse($rule->validate('<mail@cebe.cc>')->isValid());
-        $this->assertFalse($rule->validate('info@örtliches.de')->isValid());
-        $this->assertFalse($rule->validate('sam@рмкреатиф.ru')->isValid());
-        $this->assertFalse($rule->validate('ex..ample@example.com')->isValid());
-        $this->assertFalse($rule->validate(['developer@yiiframework.com'])->isValid());
+        $this->assertSame($expectedIsValid, $result->isValid());
+    }
 
+    public function validateWithAllowNameProvider(): array
+    {
+        return [
+            [new Email(allowName: true), 'sam@rmcreative.ru', true],
+            [new Email(allowName: true), '5011@gmail.com', true],
+            [new Email(allowName: true), 'rmcreative.ru', false],
+            [new Email(allowName: true), 'Carsten Brandt <mail@cebe.cc>', true],
+            [new Email(allowName: true), '"Carsten Brandt" <mail@cebe.cc>', true],
+            [new Email(allowName: true), '<mail@cebe.cc>', true],
+            [new Email(allowName: true), 'info@örtliches.de', false],
+            [new Email(allowName: true), 'üñîçøðé@üñîçøðé.com', false],
+            [new Email(allowName: true), 'sam@рмкреатиф.ru', false],
+            [new Email(allowName: true), 'Informtation info@oertliches.de', false],
+            [new Email(allowName: true), 'test@example.com', true],
+            [new Email(allowName: true), 'John Smith <john.smith@example.com>', true],
+            [
+                new Email(allowName: true),
+                '"This name is longer than 64 characters. Blah blah blah blah blah" <shortmail@example.com>',
+                true,
+            ],
+            [new Email(allowName: true), 'John Smith <example.com>', false],
+            [
+                new Email(allowName: true),
+                'Short Name <localPartMoreThan64Characters-blah-blah-blah-blah-blah-blah-blah-blah@example.com>',
+                false,
+            ],
+            [
+                new Email(allowName: true),
+                'Short Name <domainNameIsMoreThan254Characters@example-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah.com>',
+                false,
+            ],
+            [new Email(allowName: true), ['developer@yiiframework.com'], false],
+        ];
+    }
+
+    /**
+     * @dataProvider validateWithAllowNameProvider
+     */
+    public function testValidateWithAllowName(mixed $value, bool $expectedIsValid): void
+    {
         $rule = new Email(allowName: true);
+        $result = $rule->validate($value);
 
-        $this->assertTrue($rule->validate('sam@rmcreative.ru')->isValid());
-        $this->assertTrue($rule->validate('5011@gmail.com')->isValid());
-        $this->assertFalse($rule->validate('rmcreative.ru')->isValid());
-        $this->assertTrue($rule->validate('Carsten Brandt <mail@cebe.cc>')->isValid());
-        $this->assertTrue($rule->validate('"Carsten Brandt" <mail@cebe.cc>')->isValid());
-        $this->assertTrue($rule->validate('<mail@cebe.cc>')->isValid());
-        $this->assertFalse($rule->validate('info@örtliches.de')->isValid());
-        $this->assertFalse($rule->validate('üñîçøðé@üñîçøðé.com')->isValid());
-        $this->assertFalse($rule->validate('sam@рмкреатиф.ru')->isValid());
-        $this->assertFalse($rule->validate('Informtation info@oertliches.de')->isValid());
-        $this->assertTrue($rule->validate('test@example.com')->isValid());
-        $this->assertTrue($rule->validate('John Smith <john.smith@example.com>')->isValid());
-        $this->assertTrue(
-            $rule->validate(
-                '"This name is longer than 64 characters. Blah blah blah blah blah" <shortmail@example.com>'
-            )->isValid()
-        );
-        $this->assertFalse($rule->validate('John Smith <example.com>')->isValid());
-        $this->assertFalse(
-            $rule->validate(
-                'Short Name <localPartMoreThan64Characters-blah-blah-blah-blah-blah-blah-blah-blah@example.com>'
-            )->isValid()
-        );
-        $this->assertFalse(
-            $rule->validate(
-                'Short Name <domainNameIsMoreThan254Characters@example-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah-blah.com>'
-            )->isValid()
-        );
-        $this->assertFalse($rule->validate(['developer@yiiframework.com'])->isValid());
+        $this->assertSame($expectedIsValid, $result->isValid());
     }
 
     public function testValidateIdn(): void
