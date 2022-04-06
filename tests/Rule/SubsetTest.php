1f902f5831962e34bd51e633895575727a2909dc
Alexey Rogachev <arogachev90@gmail.com>
2022-04-06 16:47:31 +0600

    Refactor SubsetTest

--- tests/Rule/SubsetTest.php
+++ tests/Rule/SubsetTest.php
@@ -10,29 +10,48 @@ use PHPUnit\Framework\TestCase;
 
 class SubsetTest extends TestCase
 {
-    public function testValidateArrayValue(): void
+    public function validateArrayValueProvider(): array
     {
         $rule = new Subset(range(1, 10));
 
-        $this->assertTrue($rule->validate([1, 2, 3, 4, 5])->isValid());
-        $this->assertTrue($rule->validate([6, 7, 8, 9, 10])->isValid());
-        $this->assertFalse($rule->validate([0, 1, 2])->isValid());
-        $this->assertFalse($rule->validate([10, 11, 12])->isValid());
-        $this->assertTrue($rule->validate(['1', '2', '3', 4, 5, 6])->isValid());
+        return [
+            [$rule, [1, 2, 3, 4, 5], true],
+            [$rule, [6, 7, 8, 9, 10], true],
+            [$rule, [0, 1, 2], false],
+            [$rule, [10, 11, 12], false],
+            [$rule, ['1', '2', '3', 4, 5, 6], true],
+        ];
     }
 
-    public function testValidateSubsetArrayable(): void
+    /**
+     * @dataProvider validateArrayValueProvider
+     */
+    public function testValidateArrayValue(Subset $rule, array $value, bool $expectedIsValid): void
     {
-        // Test in array, values are arrays. IE: ['a', 'b'] subset [['a', 'b', 'c']
-        $rule = new Subset(['a', 'b', 'c']);
-        $this->assertTrue($rule->validate(['a', 'b'])->isValid());
-
-        // Test in array, values are arrays. IE: ['a', 'b'] subset [['a', 'b', 'c']
-        $rule = new Subset(['a', 'b', 'c']);
-        $this->assertTrue($rule->validate(new ArrayObject(['a', 'b']))->isValid());
+        $result = $rule->validate($value);
+        $this->assertSame($expectedIsValid, $result->isValid());
     }
 
-    public function testValidateEmpty()
+    public function validateSubsetArrayableProvider()
+    {
+        $rule = new Subset(['a', 'b', 'c']);
+
+        return [
+            [$rule, ['a', 'b']],
+            [$rule, new ArrayObject(['a', 'b'])]
+        ];
+    }
+
+    /**
+     * @dataProvider validateSubsetArrayableProvider
+     */
+    public function testValidateSubsetArrayable(Subset $rule, array|ArrayObject $value): void
+    {
+        $result = $rule->validate($value);
+        $this->assertTrue($result->isValid());
+    }
+
+    public function testValidateEmpty(): void
     {
         $rule = new Subset(range(10, 20));
         $this->assertTrue($rule->validate([])->isValid());
