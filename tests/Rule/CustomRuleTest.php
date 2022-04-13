15d51a3db6cfa4f54e7e24ab1791e77cf1d35a6c
Alexey Rogachev <arogachev90@gmail.com>
2022-04-13 14:22:22 +0600

    Add according test as well

---
+++ tests/Rule/CustomRuleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Tests\Rule;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Validator\Error;
+use Yiisoft\Validator\Result;
+use Yiisoft\Validator\Rule\Number;
+use Yiisoft\Validator\RuleInterface;
+use Yiisoft\Validator\ValidationContext;
+
+class CustomRuleTest extends TestCase
+{
+    public function testUsingCommonArgumentsViaComposition()
+    {
+        $rule = new class() implements RuleInterface
+        {
+            private Number $baseRule;
+
+            public function __construct()
+            {
+                $this->baseRule = new Number(min: -10, max: 10);
+            }
+
+            public function validate(mixed $value, ?ValidationContext $context = null): Result
+            {
+                return $this->baseRule->validate($value, $context);
+            }
+        };
+        $result = $rule->validate(20);
+        $expectedErrors = [new Error('Value must be no greater than 10.')];
+
+        $this->assertEquals($expectedErrors, $result->getErrors());
+    }
+}
