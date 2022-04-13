4d4a00d32f6f7e67f2e1c05772915cb5711a5a7a
Dmitrii Derepko <xepozz@list.ru>
2022-04-14 00:10:28 +0300

    Add test for Json rule

---
+++ tests/Rule/Json/JsonTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Tests\Rule\Json;
+
+use Yiisoft\Validator\ParametrizedRuleInterface;
+use Yiisoft\Validator\Rule\Json\Json;
+use Yiisoft\Validator\Tests\Rule\AbstractRuleTest;
+
+/**
+ * @group t2
+ */
+final class JsonTest extends AbstractRuleTest
+{
+    public function optionsDataProvider(): array
+    {
+        return [
+            [
+                new Json(),
+                [
+                    'message' => [
+                        'message' => 'The value is not JSON.',
+                    ],
+                    'skipOnEmpty' => false,
+                    'skipOnError' => false,
+                ],
+            ],
+        ];
+    }
+
+    protected function getRule(): ParametrizedRuleInterface
+    {
+        return new Json();
+    }
+}
