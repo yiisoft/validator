37f3750ea31af71d696560ec775d393a83c80315
Rustam <rmamdaminov@gmail.com>
2022-05-27 19:01:18 +0500

    Fix

--- src/Rule/EachHandler.php
+++ src/Rule/EachHandler.php
@@ -6,6 +6,7 @@ namespace Yiisoft\Validator\Rule;
 
 use InvalidArgumentException;
 use Yiisoft\Validator\Result;
+use Yiisoft\Validator\RuleInterface;
 use Yiisoft\Validator\ValidationContext;
 use Yiisoft\Validator\Exception\UnexpectedRuleException;
 
