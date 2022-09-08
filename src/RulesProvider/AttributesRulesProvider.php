6503e5278f40311269b10faf2feef1d4cf97f36d
Dmitrii Derepko <xepozz@list.ru>
2022-09-08 20:01:56 +0300

    Improve reflection usage

--- src/RulesProvider/AttributesRulesProvider.php
+++ src/RulesProvider/AttributesRulesProvider.php
@@ -48,11 +48,7 @@ final class AttributesRulesProvider implements RulesProviderInterface
         $reflection = is_object($this->source)
             ? new ReflectionObject($this->source)
             : new ReflectionClass($this->source);
-        foreach ($reflection->getProperties() as $property) {
-            if (!$this->isUseProperty($property)) {
-                continue;
-            }
-
+        foreach ($reflection->getProperties($this->propertyVisibility) as $property) {
             $attributes = $property->getAttributes(RuleInterface::class, ReflectionAttribute::IS_INSTANCEOF);
             foreach ($attributes as $attribute) {
                 $rules[$property->getName()][] = $attribute->newInstance();
@@ -60,12 +56,5 @@ final class AttributesRulesProvider implements RulesProviderInterface
         }
 
         return $rules;
-    }
-
-    private function isUseProperty(ReflectionProperty $property): bool
-    {
-        return ($property->isPublic() && ($this->propertyVisibility & ReflectionProperty::IS_PUBLIC))
-            || ($property->isPrivate() && ($this->propertyVisibility & ReflectionProperty::IS_PRIVATE))
-            || ($property->isProtected() && ($this->propertyVisibility & ReflectionProperty::IS_PROTECTED));
     }
 }
