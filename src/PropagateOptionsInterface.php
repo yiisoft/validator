66d84c2c55b339ef6333037e577f9f34f98c02b9
Alexey Rogachev <arogachev90@gmail.com>
2022-09-14 11:41:07 +0600

    Add PHPDoc [skip ci]

--- src/PropagateOptionsInterface.php
+++ src/PropagateOptionsInterface.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator;
 
+/**
+ * An interface implemented by rules that can propagate their common options (such as `skipOnEmpty`, `skipOnError`,
+ * `when`) to child rules as an alternative way of specifying them explicitly in every child rule.
+ */
 interface PropagateOptionsInterface
 {
     public function propagateOptions(): void;
