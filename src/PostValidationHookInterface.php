c6e27d09ac161f433454e76f356bd77cb60386fe
Alexey Rogachev <arogachev90@gmail.com>
2022-09-21 17:00:17 +0600

    Fix broken see tag

--- src/PostValidationHookInterface.php
+++ src/PostValidationHookInterface.php
@@ -7,7 +7,7 @@ namespace Yiisoft\Validator;
 /**
  * Allows implementing post-validation processing in the data set object itself.
  *
- * @see \Yiisoft\Form\FormModel::processValidationResult
+ * @link https://github.com/yiisoft/form/blob/d2333f9a0a77f6dcb00db1ab8ee95ec4426ea133/src/FormModel.php#L202
  */
 interface PostValidationHookInterface extends DataSetInterface
 {
