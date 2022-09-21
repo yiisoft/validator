d1524d1e6fe18578a119aa35076a9febc93e6ebd
Alexey Rogachev <arogachev90@gmail.com>
2022-09-21 12:08:12 +0600

    Fix #186: Add options' progagation for `Nested` rule (#317)

--- src/BeforeValidationInterface.php
+++ src/BeforeValidationInterface.php
@@ -7,16 +7,21 @@ namespace Yiisoft\Validator;
 use Closure;
 
 /**
- * `BeforeValidationInterface` is the interface implemented by rules that need to execute checks before the validation.
+ * `BeforeValidationInterface` is an interface implemented by rules that need to execute checks before the validation.
  */
 interface BeforeValidationInterface
 {
+    public function skipOnError(bool $value): static;
+
     public function shouldSkipOnError(): bool;
 
     /**
+     * @psalm-param Closure(mixed, ValidationContext):bool|null $value
+     */
+    public function when(?Closure $value): static;
+
+    /**
      * @psalm-return Closure(mixed, ValidationContext):bool|null
-     *
-     * @return Closure|null
      */
     public function getWhen(): ?Closure;
 }
