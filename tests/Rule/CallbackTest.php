bdd36fd1de07502da254ae20ac464a4060c65fdb
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-10-12 08:55:28 +0300

    Improve `CallbackTest`

--- tests/Rule/CallbackTest.php
+++ tests/Rule/CallbackTest.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator\Tests\Rule;
 
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Validator\Exception\InvalidCallbackReturnTypeException;
 use Yiisoft\Validator\Result;
-use Yiisoft\Validator\Rule\AtLeast;
 use Yiisoft\Validator\Rule\Callback;
-use Yiisoft\Validator\SerializableRuleInterface;
+use Yiisoft\Validator\Rule\CallbackHandler;
+use Yiisoft\Validator\Tests\Stub\FakeValidatorFactory;
+use Yiisoft\Validator\Tests\Support\RuleWithCustomHandler;
 use Yiisoft\Validator\ValidationContext;
+use Yiisoft\Validator\Validator;
 
-final class CallbackTest extends AbstractRuleTest
+final class CallbackTest extends TestCase
 {
     public function testGetName(): void
     {
@@ -18,7 +23,7 @@ final class CallbackTest extends AbstractRuleTest
         $this->assertSame('callback', $rule->getName());
     }
 
-    public function optionsDataProvider(): array
+    public function dataOptions(): array
     {
         return [
             [
@@ -43,8 +48,132 @@ final class CallbackTest extends AbstractRuleTest
         ];
     }
 
-    protected function getRule(): SerializableRuleInterface
+    /**
+     * @dataProvider dataOptions
+     */
+    public function testOptions(Callback $rule, array $expectedOptions): void
     {
-        return new AtLeast([]);
+        $options = $rule->getOptions();
+        $this->assertSame($expectedOptions, $options);
+    }
+
+    public function dataValidationPassed(): array
+    {
+        return [
+            [
+                42,
+                [
+                    new Callback(static function (mixed $value, object $rule, ValidationContext $context): Result {
+                        $result = new Result();
+                        if ($value !== 42) {
+                            $result->addError('Value should be 42!');
+                        }
+
+                        return $result;
+                    })
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataValidationPassed
+     */
+    public function testValidationPassed(mixed $data, array $rules): void
+    {
+        $result = $this->createValidator()->validate($data, $rules);
+
+        $this->assertTrue($result->isValid());
+    }
+
+    public function dataValidationFailed(): array
+    {
+        return [
+            [
+                41,
+                [
+                    new Callback(static function (mixed $value, object $rule, ValidationContext $context): Result {
+                        $result = new Result();
+                        if ($value !== 42) {
+                            $result->addError('Value should be 42!');
+                        }
+
+                        return $result;
+                    })
+                ],
+                ['' => ['Value should be 42!']],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataValidationFailed
+     */
+    public function testValidationFailed(mixed $data, array $rules, array $errorMessagesIndexedByPath): void
+    {
+        $result = $this->createValidator()->validate($data, $rules);
+
+        $this->assertFalse($result->isValid());
+        $this->assertSame($errorMessagesIndexedByPath, $result->getErrorMessagesIndexedByPath());
+    }
+
+    public function testCustomErrorMessage(): void
+    {
+        $data = 41;
+        $rules = [
+            new Callback(static function (mixed $value, object $rule, ValidationContext $context): Result {
+                $result = new Result();
+                if ($value !== 42) {
+                    $result->addError('Custom error');
+                }
+
+                return $result;
+            })
+        ];
+
+        $result = $this->createValidator()->validate($data, $rules);
+
+        $this->assertFalse($result->isValid());
+        $this->assertSame(
+            ['' => ['Custom error']],
+            $result->getErrorMessagesIndexedByPath()
+        );
+    }
+
+    public function testThrowExceptionWithInvalidReturn(): void
+    {
+        $rule = new Callback(
+            static fn (mixed $value, object $rule, ValidationContext $context): string => 'invalid return'
+        );
+        $validator = $this->createValidator();
+
+        $this->expectException(InvalidCallbackReturnTypeException::class);
+        $validator->validate(null, [$rule]);
+    }
+
+    public function testValidateUsingMethodOutsideAttributeScope(): void
+    {
+        $rule = new Callback(method: 'validateName');
+        $validator = $this->createValidator();
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Using method outside of attribute scope is prohibited.');
+        $validator->validate(null, [$rule]);
+    }
+
+    public function testDifferentRuleInHandler(): void
+    {
+        $rule = new RuleWithCustomHandler(CallbackHandler::class);
+        $validator = $this->createValidator();
+
+        $this->expectExceptionMessageMatches(
+            '/.*' . preg_quote(Callback::class) . '.*' . preg_quote(RuleWithCustomHandler::class) . '.*/'
+        );
+        $validator->validate([], [$rule]);
+    }
+
+    private function createValidator(): Validator
+    {
+        return FakeValidatorFactory::make();
     }
 }
