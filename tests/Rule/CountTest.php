210d0b206fb945ee09121b360dfafcc6ceb5b0dd
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-10-12 09:43:22 +0300

    Improve `CountTest`

--- tests/Rule/CountTest.php
+++ tests/Rule/CountTest.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator\Tests\Rule;
 
+use Countable;
 use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use Yiisoft\Validator\DataSet\MixedDataSet;
 use Yiisoft\Validator\Rule\Count;
-use Yiisoft\Validator\SerializableRuleInterface;
+use Yiisoft\Validator\Tests\Stub\FakeValidatorFactory;
+use Yiisoft\Validator\Validator;
 
-final class CountTest extends AbstractRuleTest
+final class CountTest extends TestCase
 {
     public function testGetName(): void
     {
@@ -16,20 +21,15 @@ final class CountTest extends AbstractRuleTest
         $this->assertSame('count', $rule->getName());
     }
 
-    public function optionsDataProvider(): array
+    public function dataOptions(): array
     {
         return [
             [
                 new Count(min: 3),
                 [
-                    'skipOnEmpty' => false,
-                    'skipOnError' => false,
                     'min' => 3,
                     'max' => null,
                     'exactly' => null,
-                    'message' => [
-                        'message' => 'This value must be an array or implement \Countable interface.',
-                    ],
                     'lessThanMinMessage' => [
                         'message' => 'This value must contain at least {min, number} {min, plural, one{item} ' .
                             'other{items}}.',
@@ -45,12 +45,125 @@ final class CountTest extends AbstractRuleTest
                             'other{items}}.',
                         'parameters' => ['exactly' => null],
                     ],
+                    'message' => [
+                        'message' => 'This value must be an array or implement \Countable interface.',
+                    ],
+                    'skipOnEmpty' => false,
+                    'skipOnError' => false,
                 ],
             ],
         ];
     }
 
-    public function initWithMinAndMaxAndExactlyDataProvider(): array
+    /**
+     * @dataProvider dataOptions
+     */
+    public function testOptions(Count $rule, array $expectedOptions): void
+    {
+        $options = $rule->getOptions();
+        $this->assertSame($expectedOptions, $options);
+    }
+
+    public function dataValidationPassed(): array
+    {
+        return [
+            [[0, 0, 0], [new Count(min: 3)]],
+            [[0, 0, 0, 0], [new Count(min: 3)]],
+            [[0, 0, 0], [new Count(exactly: 3)]],
+            [[], [new Count(max: 3)]],
+            [[0, 0], [new Count(max: 3)]],
+            [[0, 0, 0], [new Count(max: 3)]],
+            [
+                new MixedDataSet(
+                    new class () implements Countable {
+                        protected int $myCount = 3;
+
+                        public function count(): int
+                        {
+                            return $this->myCount;
+                        }
+                    }
+                ),
+                [new Count(min: 3)],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataValidationPassed
+     */
+    public function testValidationPassed(mixed $data, array $rules): void
+    {
+        $result = $this->createValidator()->validate($data, $rules);
+
+        $this->assertTrue($result->isValid());
+    }
+
+    public function dataValidationFailed(): array
+    {
+        $lessThanMinmessage = 'This value must contain at least 3 items.';
+        $greaterThanMaxMessage = 'This value must contain at most 3 items.';
+
+        return [
+            [1, [new Count(min: 3)], ['' => ['This value must be an array or implement \Countable interface.']]],
+            [[1], [new Count(min: 3)], ['' => [$lessThanMinmessage]]],
+            [[], [new Count(min: 3)], ['' => [$lessThanMinmessage]]],
+            [[0, 0], [new Count(min: 3)], ['' => [$lessThanMinmessage]]],
+            [[1.1], [new Count(min: 3)], ['' => [$lessThanMinmessage]]],
+            [[''], [new Count(min: 3)], ['' => [$lessThanMinmessage]]],
+            [['some string'], [new Count(min: 3)], ['' => [$lessThanMinmessage]]],
+            [[new stdClass()], [new Count(min: 3)], ['' => [$lessThanMinmessage]]],
+            // https://www.php.net/manual/ru/class.countable.php
+            [
+                [
+                    new class () {
+                        protected int $myCount = 3;
+
+                        public function count(): int
+                        {
+                            return $this->myCount;
+                        }
+                    },
+                ],
+                [new Count(min: 3)],
+                ['' => [$lessThanMinmessage]],
+            ],
+            [[0, 0, 0, 0], [new Count(max: 3)], ['' => [$greaterThanMaxMessage]]],
+        ];
+    }
+
+    /**
+     * @dataProvider dataValidationFailed
+     */
+    public function testValidationFailed(mixed $data, array $rules, array $errorMessagesIndexedByPath): void
+    {
+        $result = $this->createValidator()->validate($data, $rules);
+
+        $this->assertFalse($result->isValid());
+        $this->assertSame($errorMessagesIndexedByPath, $result->getErrorMessagesIndexedByPath());
+    }
+
+    public function dataCustomErrorMessage(): array
+    {
+        return [
+            [[0, 0, 0, 0], [new Count(max: 3, greaterThanMaxMessage: 'Custom message.')], ['' => ['Custom message.']]],
+            [[0, 0, 0, 0], [new Count(exactly: 3, notExactlyMessage: 'Custom message.')], ['' => ['Custom message.']]],
+            [[0, 0], [new Count(min: 3, lessThanMinMessage: 'Custom message.')], ['' => ['Custom message.']]],
+        ];
+    }
+
+    /**
+     * @dataProvider dataCustomErrorMessage
+     */
+    public function testCustomErrorMessage(mixed $data, array $rules, array $errorMessagesIndexedByPath): void
+    {
+        $result = $this->createValidator()->validate($data, $rules);
+
+        $this->assertFalse($result->isValid());
+        $this->assertSame($errorMessagesIndexedByPath, $result->getErrorMessagesIndexedByPath());
+    }
+
+    public function dataInitWithMinAndMaxAndExactly(): array
     {
         return [
             [['min' => 3, 'exactly' => 3]],
@@ -60,13 +173,12 @@ final class CountTest extends AbstractRuleTest
     }
 
     /**
-     * @dataProvider initWithMinAndMaxAndExactlyDataProvider
+     * @dataProvider dataInitWithMinAndMaxAndExactly
      */
     public function testInitWithMinAndMaxAndExactly(array $arguments): void
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('$exactly is mutually exclusive with $min and $max.');
-
         new Count(...$arguments);
     }
 
@@ -74,7 +186,6 @@ final class CountTest extends AbstractRuleTest
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Use $exactly instead.');
-
         new Count(min: 3, max: 3);
     }
 
@@ -82,12 +193,11 @@ final class CountTest extends AbstractRuleTest
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('At least one of these attributes must be specified: $min, $max, $exactly.');
-
         new Count();
     }
 
-    protected function getRule(): SerializableRuleInterface
+    private function createValidator(): Validator
     {
-        return new Count(min: 1);
+        return FakeValidatorFactory::make();
     }
 }
