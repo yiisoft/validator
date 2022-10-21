9411f2a4b355a18ae2e1b9605e7f2a2cc49f49f8
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-10-21 14:36:06 +0300

    `Required` rule: remove `SkipOnEmptyInterface` and add `emptyCallback` option (#342)
    * `Required`: remove `SkipOnEmptyInterface` and add `emptyCallback` option
    
    * Apply fixes from StyleCI
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>
    
    * Update README.md
    
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>
    
    * Update README.md
    
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>
    
    * Fix README
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>
    Co-authored-by: Alexey Rogachev <arogachev90@gmail.com>
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- README.md
+++ README.md
@@ -142,7 +142,10 @@ new Number(asInteger: true, max: 100, skipOnError: true)
 #### Skipping empty values
 
 By default, missing and empty values are validated (if the value is missing, it's considered `null`). That is
-undesirable if you need to allow not specifying a field. To change this behavior, use `skipOnEmpty: true`:
+undesirable if you need a field to be optional. To change this behavior, use `skipOnEmpty: true`.
+
+Note that not every rule has this option, but only the ones that implement `Yiisoft\Validator\SkipOnEmptyInterface`. For 
+example, `Required` rule doesn't. For more details see "Requiring values" section.
 
 ```php
 use Yiisoft\Validator\Rule\Number;
@@ -698,6 +701,19 @@ final class Post
     #[HasLength(max: 255, formatter: new CustomFormatter())]
     private string $title;
 }
+```
+
+### Requiring values
+
+Use `Yiisoft\Validator\Rule\Required` rule to make sure a value is present. What values are considered empty can be 
+customized via `$emptyCallback` option. Normalization is not performed, so only a callable or special class is needed. 
+For more details see "Skipping empty values" section.
+
+```php
+use Yiisoft\Validator\Rule\Required;
+use Yiisoft\Validator\SkipOnEmptyCallback\SkipOnNull;
+
+$rules = [new Required(emptyCallback: new SkipOnNull())];
 ```
 
 ### Conditional validation
--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -862,7 +862,7 @@ class ValidatorTest extends TestCase
             'null' => [
                 null,
                 new class () {
-                    #[Required]
+                    #[Number]
                     public ?string $name = null;
                 },
                 false,
@@ -870,7 +870,7 @@ class ValidatorTest extends TestCase
             'false' => [
                 false,
                 new class () {
-                    #[Required]
+                    #[Number]
                     public ?string $name = null;
                 },
                 false,
@@ -878,7 +878,7 @@ class ValidatorTest extends TestCase
             'true' => [
                 true,
                 new class () {
-                    #[Required]
+                    #[Number]
                     public ?string $name = null;
                 },
                 true,
@@ -886,7 +886,7 @@ class ValidatorTest extends TestCase
             'callable' => [
                 new SkipOnNull(),
                 new class () {
-                    #[Required]
+                    #[Number]
                     public ?string $name = null;
                 },
                 true,
@@ -894,7 +894,7 @@ class ValidatorTest extends TestCase
             'do-not-override-rule' => [
                 false,
                 new class () {
-                    #[Required(skipOnEmpty: true)]
+                    #[Number(skipOnEmpty: true)]
                     public string $name = '';
                 },
                 true,
