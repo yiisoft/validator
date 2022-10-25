f16dc430b342a514afa43edbb1413f7516777ba2
Alexey Rogachev <arogachev90@gmail.com>
2022-10-25 22:18:40 +0600

    Fix concat mutant in max adress length

--- src/Rule/EmailHandler.php
+++ src/Rule/EmailHandler.php
@@ -53,7 +53,10 @@ final class EmailHandler implements RuleHandlerInterface
                 // The maximum total length of a user name or other local-part is 64 octets. RFC 5322 section 4.5.3.1.1
                 // http://tools.ietf.org/html/rfc5321#section-4.5.3.1.1
                 $valid = false;
-            } elseif (is_string($matches['local']) && strlen($matches['local'] . '@' . $matches['domain']) > 254) {
+            } elseif (
+                is_string($matches['local']) &&
+                strlen($matches['local']) + strlen((string) $matches['domain']) > 253
+            ) {
                 // There is a restriction in RFC 2821 on the length of an address in MAIL and RCPT commands
                 // of 254 characters. Since addresses that do not fit in those fields are not normally useful, the
                 // upper limit on address lengths should normally be considered to be 254.
