83778ec36d1dc05ce2d491678af7c92c016da8c4
Alexey Rogachev <arogachev90@gmail.com>
2022-10-27 18:13:55 +0600

    Fix tests regression in LimitHandlerTrait

--- src/Rule/Trait/LimitHandlerTrait.php
+++ src/Rule/Trait/LimitHandlerTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Validator\Rule\Trait;
 
 use Yiisoft\Validator\Result;
+use Yiisoft\Validator\RuleInterface;
 
 /**
  * A trait attachable to a handler of rule with limits.
@@ -14,13 +15,13 @@ trait LimitHandlerTrait
     /**
      * Runs limits specific validation.
      *
-     * @param LimitTrait $rule A rule matching to this handler.
+     * @param RuleInterface|LimitTrait $rule A rule matching to this handler.
      * @param int $number A validated number to compare with set limits.
      * @param Result $result Result for adding errors.
      *
      * @see LimitTrait for information about limits and messages.
      */
-    private function validateLimits(LimitTrait $rule, int $number, Result $result): void
+    private function validateLimits(object $rule, int $number, Result $result): void
     {
         if ($rule->getExactly() !== null && $number !== $rule->getExactly()) {
             $result->addError($rule->getNotExactlyMessage(), ['exactly' => $rule->getExactly()]);
