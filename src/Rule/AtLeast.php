8f836b277bb82756ce9bb4bcb13c49fd2013df6f
Alexey Rogachev <arogachev90@gmail.com>
2022-11-02 17:50:07 +0600

    Add incorrect input message, more specific type for attributes

--- src/Rule/AtLeast.php
+++ src/Rule/AtLeast.php
@@ -29,14 +29,19 @@ final class AtLeast implements SerializableRuleInterface, SkipOnErrorInterface, 
      * @param string[] $attributes The list of required attributes that will be checked.
      * @param int $min The minimum required quantity of filled attributes to pass the validation.
      * Defaults to 1.
+     * @param string $incorrectInputMessage = 'Value must be array or iterable.',
      * @param string $message Message to display in case of error.
      * @param bool|callable|null $skipOnEmpty
      * @param bool $skipOnError
      * @param Closure(mixed, ValidationContext):bool|null $when
      */
     public function __construct(
+        /**
+         * @var string[]
+         */
         private array $attributes,
         private int $min = 1,
+        private string $incorrectInputMessage = 'Value must be an array or an object.',
         private string $message = 'The model is not valid. Must have at least "{min}" filled attributes.',
         private mixed $skipOnEmpty = null,
         private bool $skipOnError = false,
@@ -62,6 +67,11 @@ final class AtLeast implements SerializableRuleInterface, SkipOnErrorInterface, 
         return $this->min;
     }
 
+    public function getIncorrectInputMessage(): string
+    {
+        return $this->incorrectInputMessage;
+    }
+
     public function getMessage(): string
     {
         return $this->message;
@@ -72,6 +82,7 @@ final class AtLeast implements SerializableRuleInterface, SkipOnErrorInterface, 
         return [
             'attributes' => $this->attributes,
             'min' => $this->min,
+            'incorrectInputMessage' => $this->incorrectInputMessage,
             'message' => [
                 'message' => $this->message,
                 'parameters' => ['min' => $this->min],
