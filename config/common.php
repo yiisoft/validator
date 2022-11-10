55aa31328d3ee3f40cebb7e927c226ef67355715
Rustam Mamadaminov <rmamdaminov@gmail.com>
2022-11-10 08:00:53 +0500

    Adapt translator (#366)

--- config/common.php
+++ config/common.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
 use Yiisoft\Translator\CategorySource;
-use Yiisoft\Translator\MessageFormatterInterface;
 use Yiisoft\Translator\SimpleMessageFormatter;
 use Yiisoft\Validator\IdMessageReader;
 use Yiisoft\Validator\RuleHandlerResolverInterface;
@@ -17,17 +16,18 @@ use Yiisoft\Validator\ValidatorInterface;
 return [
     ValidatorInterface::class => Validator::class,
     RuleHandlerResolverInterface::class => SimpleRuleHandlerContainer::class,
-    'validator.categorySource' => static function (ContainerInterface $container) use ($params) {
-        $messageSource = $container->get('validator.messageSource');
-        $messageFormatter = $container->has(MessageFormatterInterface::class)
-            ? $container->get(MessageFormatterInterface::class)
-            : new SimpleMessageFormatter();
+    'validator.categorySource' => [
+        'definition' => static function (ContainerInterface $container) use ($params) {
+            $messageSource = $container->get('validator.messageSource');
+            $messageFormatter = new SimpleMessageFormatter();
 
-        return new CategorySource(
-            $params['yiisoft/translator']['validatorCategory'],
-            $messageSource,
-            $messageFormatter,
-        );
-    },
+            return new CategorySource(
+                $params['yiisoft/translator']['validatorCategory'],
+                $messageSource,
+                $messageFormatter,
+            );
+        },
+        'tags' => ['translation.categorySource'],
+    ],
     'validator.messageSource' => IdMessageReader::class,
 ];
--- config/params.php
+++ config/params.php
@@ -2,15 +2,9 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Definitions\Reference;
 
 return [
     'yiisoft/translator' => [
         'validatorCategory' => 'validator',
-        'categorySources' => [
-            // You can add categories from your application and additional modules using `Reference::to` below
-            // Reference::to(ApplicationCategorySource::class),
-            Reference::to('validator.categorySource'),
-        ],
     ],
 ];
