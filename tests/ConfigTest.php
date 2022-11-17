985f91ca02441fdece961f7cd106953d4fb88316
Alexey Rogachev <arogachev90@gmail.com>
2022-11-17 19:05:54 +0600

    Increase coverage for config tests

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -7,8 +7,11 @@ namespace Yiisoft\Validator\Tests;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Di\Container;
 use Yiisoft\Di\ContainerConfig;
+use Yiisoft\Translator\CategorySource;
 use Yiisoft\Validator\Validator;
 use Yiisoft\Validator\ValidatorInterface;
+
+use function dirname;
 
 final class ConfigTest extends TestCase
 {
@@ -17,22 +20,50 @@ final class ConfigTest extends TestCase
         $container = $this->createContainer();
 
         $validator = $container->get(ValidatorInterface::class);
-
         $this->assertInstanceOf(Validator::class, $validator);
+
+        $translationCategorySource = $this->getTranslationCategorySource($container);
+        $this->assertSame('yii-validator', $translationCategorySource->getName());
     }
 
-    private function createContainer(?array $params = null): Container
+    public function testCustomTranslationCategory(): void
     {
-        return new Container(
-            ContainerConfig::create()->withDefinitions($this->getCommonDefinitions($params))
-        );
+        $params = [
+            'yiisoft/validator' => [
+                'translation.category' => 'yii-validator-custom',
+            ],
+        ];
+        $container = $this->createContainer($params);
+
+        $translationCategorySource = $this->getTranslationCategorySource($container);
+        $this->assertSame('yii-validator-custom', $translationCategorySource->getName());
     }
 
-    private function getCommonDefinitions(?array $params): array
+    private function getTranslationCategorySource(Container $container): CategorySource
+    {
+        /** @var CategorySource[] $translationCategorySource */
+        $translationCategorySources = $container->get('tag@translation.categorySource');
+        $this->assertCount(1, $translationCategorySources);
+
+        $translationCategorySource = $translationCategorySources[0];
+        $this->assertInstanceOf(CategorySource::class, $translationCategorySource);
+
+        return $translationCategorySource;
+    }
+
+    private function createContainer(array|null $params = null): Container
+    {
+        $config = ContainerConfig::create()->withDefinitions($this->getCommonDefinitions($params));
+
+        return new Container($config);
+    }
+
+    private function getCommonDefinitions(array|null $params): array
     {
         if ($params === null) {
             $params = $this->getParams();
         }
+
         return require dirname(__DIR__) . '/config/common.php';
     }
 
