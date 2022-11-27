470c380c3b1422b0e8cdd85b10c3d30aefc90352
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-11-27 20:04:20 +0300

    Extract object parsing to separate class (#373)
    * Extract object parsing to separate class
    
    * skip static properties
    
    * fix
    
    * minor fixes and more tests
    
    * Apply fixes from StyleCI
    
    * improve tests
    
    * Apply fixes from StyleCI
    
    * fix
    
    Co-authored-by: Alexey Rogachev <arogachev90@gmail.com>
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ tests/Helper/ObjectParserTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Tests\Helper;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Validator\Helper\ObjectParser;
+
+final class ObjectParserTest extends TestCase
+{
+    public function dataSkipStaticProperties(): array
+    {
+        return [
+            [
+                ['a' => 4, 'b' => 2],
+                new class () {
+                    public int $a = 4;
+                    public static int $b = 2;
+                },
+                false,
+            ],
+            [
+                ['a' => 4, 'c' => 'hello'],
+                new class () {
+                    public int $a = 4;
+                    public static int $b = 2;
+                    public string $c = 'hello';
+                },
+                true,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataSkipStaticProperties
+     */
+    public function testSkipStaticProperties(array $expectedData, object $object, bool $skipStaticProperties): void
+    {
+        $parser = new ObjectParser($object, skipStaticProperties: $skipStaticProperties);
+
+        $this->assertSame($expectedData, $parser->getData());
+    }
+
+    public function testSkipStaticPropertiesDefault(): void
+    {
+        $object = new class () {
+            public int $a = 4;
+            public static int $b = 2;
+        };
+
+        $parser = new ObjectParser($object);
+
+        $this->assertSame(['a' => 4, 'b' => 2], $parser->getData());
+    }
+}
