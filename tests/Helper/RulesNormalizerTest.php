b62a309d8b36f251d13d0b6246c5e24ad12bef3b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-11-28 12:17:29 +0300

    more tests

--- tests/Helper/RulesNormalizerTest.php
+++ tests/Helper/RulesNormalizerTest.php
@@ -5,14 +5,45 @@ declare(strict_types=1);
 namespace Yiisoft\Validator\Tests\Helper;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionProperty;
 use Yiisoft\Validator\Helper\RulesNormalizer;
+use Yiisoft\Validator\Tests\Support\Data\ObjectWithDifferentPropertyVisibility;
 
 final class RulesNormalizerTest extends TestCase
 {
     public function dataNormalize(): array
     {
         return [
-            'null' => [[], null, 0, null],
+            'null' => [[], null],
+            'default-property-visibility' => [
+                [
+                    'name' => ['required'],
+                    'age' => ['number'],
+                    'number' => ['number'],
+                ],
+                new ObjectWithDifferentPropertyVisibility(),
+            ],
+            'private-properties' => [
+                [
+                    'number' => ['number'],
+                ],
+                new ObjectWithDifferentPropertyVisibility(),
+                ReflectionProperty::IS_PRIVATE,
+            ],
+            'protected-properties' => [
+                [
+                    'age' => ['number'],
+                ],
+                new ObjectWithDifferentPropertyVisibility(),
+                ReflectionProperty::IS_PROTECTED,
+            ],
+            'public-properties' => [
+                [
+                    'name' => ['required'],
+                ],
+                new ObjectWithDifferentPropertyVisibility(),
+                ReflectionProperty::IS_PUBLIC,
+            ],
         ];
     }
 
@@ -22,10 +53,20 @@ final class RulesNormalizerTest extends TestCase
     public function testNormalizeWithArrayResult(
         array $expected,
         iterable|object|string|null $rules,
-        int $propertyVisibility,
-        mixed $data
+        ?int $propertyVisibility = null,
+        mixed $data = null
     ): void {
-        $result = RulesNormalizer::normalize($rules, $propertyVisibility, $data);
+        $rules = $propertyVisibility === null
+            ? RulesNormalizer::normalize(rules: $rules, data: $data)
+            : RulesNormalizer::normalize(rules: $rules, propertyVisibility: $propertyVisibility, data: $data);
+
+        $result = [];
+        foreach ($rules as $attributeName => $attributeRules) {
+            $result[$attributeName] = [];
+            foreach ($attributeRules as $rule) {
+                $result[$attributeName][] = $rule->getName();
+            }
+        }
 
         $this->assertSame($expected, $result);
     }
