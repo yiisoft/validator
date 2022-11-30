6e3993917376483263a1ff3b0aa63b9abb68b650
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-11-30 09:17:56 +0300

    Revert support object and class string as `$rules` in `Validator::validate()`

--- src/ValidatorInterface.php
+++ src/ValidatorInterface.php
@@ -7,7 +7,7 @@ namespace Yiisoft\Validator;
 /**
  * Validator validates {@link DataSetInterface} against rules set for data set attributes.
  *
- * @psalm-type RulesType = null|RuleInterface|RulesProviderInterface|callable|iterable<RuleInterface|RuleInterface[]|callable|callable[]>
+ * @psalm-type RulesType = null|class-string|object|callable|iterable<RuleInterface|RuleInterface[]|callable|callable[]>
  */
 interface ValidatorInterface
 {
@@ -17,7 +17,7 @@ interface ValidatorInterface
      * @param DataSetInterface|mixed|RulesProviderInterface $data Data set to validate. If {@see RulesProviderInterface}
      * instance provided and rules are not specified explicitly, they are read from the
      * {@see RulesProviderInterface::getRules()}.
-     * @param callable|iterable|object|null $rules Rules to apply. If specified, rules are not read from data set even if it is
+     * @param callable|iterable|object|string|null $rules Rules to apply. If specified, rules are not read from data set even if it is
      * an instance of {@see RulesProviderInterface}.
      * @param ValidationContext|null $context Validation context that may take into account when performing validation.
      *
@@ -25,7 +25,7 @@ interface ValidatorInterface
      */
     public function validate(
         mixed $data,
-        iterable|object|callable|null $rules = null,
+        callable|iterable|object|string|null $rules = null,
         ?ValidationContext $context = null
     ): Result;
 }
