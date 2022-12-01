da956fa476962dadfad24d1b77ddd064eb056cd2
Alexey Rogachev <arogachev90@gmail.com>
2022-12-01 19:06:05 +0600

    Add docs to SkipOnErrorTrait

--- src/Rule/Trait/SkipOnErrorTrait.php
+++ src/Rule/Trait/SkipOnErrorTrait.php
@@ -4,8 +4,29 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator\Rule\Trait;
 
+/**
+ * An implementation for {@see SkipOnErrorInterface} intended to be included in rules. Requires an additional private
+ * class property `$skipOnError`. In package rules it's `false` by default:
+ *
+ * ```php
+ * public function __construct(
+ *     // ...
+ *     private bool $skipOnError = false,
+ *     // ...
+ * ) {
+ * }
+ * ```
+ */
 trait SkipOnErrorTrait
 {
+    /**
+     * A setter to change `$skipOnError` property. Implemented following immutability concept.
+     *
+     * @param bool $value A new value. `true` means to skip the current rule when the previous one errored and `false` -
+     * do not skip.
+     *
+     * @return $this The new instance with a changed value.
+     */
     public function skipOnError(bool $value): static
     {
         $new = clone $this;
@@ -13,6 +34,12 @@ trait SkipOnErrorTrait
         return $new;
     }
 
+    /**
+     * A getter for `$skipOnError` property.
+     *
+     * @return bool Current value. `true` means to skip the current rule when the previous one errored  and `false` - do
+     * not skip.
+     */
     public function shouldSkipOnError(): bool
     {
         return $this->skipOnError;
