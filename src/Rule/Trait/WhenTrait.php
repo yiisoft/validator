a27dbf5ed1f61eea9e5cb6d13e4397b65b7d421d
Alexey Rogachev <arogachev90@gmail.com>
2022-12-02 17:12:59 +0600

    Docs, part 4

--- src/Rule/Trait/WhenTrait.php
+++ src/Rule/Trait/WhenTrait.php
@@ -8,12 +8,31 @@ use Closure;
 use Yiisoft\Validator\WhenInterface;
 
 /**
+ * An implementation for {@see WhenInterface} intended to be included in rules. Requires an additional private class
+ * property `$when`. In package rules it's `null` by default:
+ *
+ * ```php
+ * public function __construct(
+ *     // ...
+ *     private Closure|null $when = null
+ *     // ...
+ * ) {
+ * }
+ * ```
+ *
  * @psalm-import-type WhenType from WhenInterface
  */
 trait WhenTrait
 {
     /**
-     * @psalm-param WhenType $value
+     * An immutable setter to change `$when` property.
+     *
+     * @psalm-param WhenType $value A new value:
+     *
+     * - `null` - always apply the validation.
+     * - `callable` - apply the validation depending on a return value: `true` - apply, `false` - do not apply.
+     *
+     * @return $this The new instance with a changed value.
      */
     public function when(Closure|null $value): static
     {
@@ -23,7 +42,12 @@ trait WhenTrait
     }
 
     /**
-     * @psalm-return WhenType
+     * A getter for `$when` property.
+     *
+     * @psalm-return WhenType Current value:
+     *
+     * - `null` - always apply the validation.
+     * - `callable` - apply the validation depending on a return value: `true` - apply, `false` - do not apply.
      */
     public function getWhen(): Closure|null
     {
