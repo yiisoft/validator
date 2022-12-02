e881ec9aa3f6fc1729a93ec2a883249f19884f4b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-12-02 12:40:08 +0300

    Fix config test

--- tests/TestEnvironments/WithoutIntl/ConfigTest.php
+++ tests/TestEnvironments/WithoutIntl/ConfigTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Validator\Tests\TestEnvironments\WithoutIntl;
 
 use Yiisoft\Translator\CategorySource;
-use Yiisoft\Translator\SimpleMessageFormatter;
+use Yiisoft\Translator\MessageFormatterInterface;
 use Yiisoft\Validator\Tests\BaseConfigTest;
 
 final class ConfigTest extends BaseConfigTest
@@ -14,13 +14,21 @@ final class ConfigTest extends BaseConfigTest
     {
         $container = $this->createContainer();
 
+        $customFormatter = new class() implements MessageFormatterInterface {
+            public function format(string $message, array $parameters, string $locale): string
+            {
+                return 'test';
+            }
+        };
+
         /** @var CategorySource $translationCategorySource */
         $translationCategorySource = $container->get('tag@translation.categorySource')[0];
         $message = '{n, selectordinal, one{#-one} two{#-two} few{#-few} other{#-other}}';
+
         // The default formatter argument is ignored in favor of formatter set in config.
         $this->assertSame(
             '1',
-            $translationCategorySource->format($message, ['n' => 1], 'en', new SimpleMessageFormatter()),
+            $translationCategorySource->format($message, ['n' => 1], 'en', $customFormatter),
         );
     }
 }
