15124f83e442407e3283ff0f238c83ea1677299c
Alexey Rogachev <arogachev90@gmail.com>
2022-12-02 17:19:28 +0600

    Docs, part 2 (#408)
    * Initial commit
    
    * Fix Psalm errors
    
    * Start to add docs for RuleInterface
    
    * Add docs to RuleInterface
    
    * Add client-side validation mention
    
    * Add return tags
    
    * Add docs to SerializableRuleInterface
    
    * Rename SerializableRuleInterface to RuleWithOptionsInterface
    
    * Apply WhenType to other places
    
    * Apply fixes from StyleCI
    
    * Sync with master
    
    * Remove "This is possible" + grammar checker fix
    
    * Add docs to SkipOnEmptyInterface
    
    * Apply fixes from StyleCI
    
    * Updates based on review + fix ambiguous behavior in getting option
    
    * Apply fixes from StyleCI
    
    * Fix coverage
    
    * Change -> Changes
    
    * Replace constructor mention [skip ci]
    
    * Make consistent with previous PR suggestions
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/SkipOnEmptyInterface.php
+++ src/SkipOnEmptyInterface.php
@@ -4,9 +4,71 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator;
 
+use Yiisoft\Validator\EmptyCriteria\NeverEmpty;
+use Yiisoft\Validator\EmptyCriteria\WhenEmpty;
+use Yiisoft\Validator\EmptyCriteria\WhenNull;
+use Yiisoft\Validator\Rule\Trait\SkipOnEmptyTrait;
+
+/**
+ * An optional interface for rules to implement. It allows skipping validation for a rule when the validated value is
+ * "empty".
+ *
+ * The package ships with {@see SkipOnEmptyTrait} which already implements that interface. All you have to do is include
+ * it in the rule class along with the interface.
+ */
 interface SkipOnEmptyInterface
 {
+    /**
+     * Changes current "skip on empty" value. Must be immutable.
+     *
+     * @param bool|callable|null $value A new value.
+     *
+     * @return $this The new instance of a rule with a changed value.
+     */
     public function skipOnEmpty(bool|callable|null $value): static;
 
+    /**
+     * Returns current "skip on empty" value.
+     *
+     * During pre-validation phase it will be normalized to an "empty criteria" - a callable identifying when and which
+     * values exactly must be considered as empty and skipped or not skipped at all.
+     *
+     * @return bool|callable|null A raw non-normalized value:
+     *
+     * - `null` - a {@see Validator::$defaultSkipOnEmptyCriteria} is used. If it's `null` there too, it's equivalent to
+     * `false` (see below).
+     * `false` - never skip a rule (the validated value is always considered as not empty). Matching criteria -
+     * {@see NeverEmpty}.
+     * - `true` - skip a rule when the validated value is empty: either not passed at all, `null`, an empty string (not
+     * trimmed by default) or an empty array. Matching criteria - {@see WhenEmpty}.
+     * - `callable` - skip a rule when evaluated to `true`.
+     *
+     * Examples of custom callables with built-in criteria:
+     *
+     * - `new WhenNull()` - less used built-in criteria ({@see WhenNull}). Skip a rule only when the validated value is
+     * `null`.
+     * - `new WhenEmpty(trimString: true)` - built-in criteria with custom arguments ({@see WhenEmpty}).
+     *
+     * A custom callable for skipping only when a value is zero:
+     *
+     * ```php
+     * static function (mixed $value, bool $isAttributeMissing): bool {
+     * {
+     *     return $value === 0;
+     * }
+     *```
+     *
+     * An equivalent class implementing `__invoke()` magic method:
+     *
+     * ```php
+     * final class SkipOnZero
+     * {
+     *     public function __invoke(mixed $value, bool $isAttributeMissing): bool
+     *     {
+     *         return $value === 0;
+     *     }
+     * }
+     * ```
+     */
     public function getSkipOnEmpty(): bool|callable|null;
 }
