8da4dceb97908fa67ad6e9592e2950f4201be221
Alexey Rogachev <arogachev90@gmail.com>
2022-12-07 18:10:50 +0600

    Docs, part 7

--- src/DataSetInterface.php
+++ src/DataSetInterface.php
@@ -4,17 +4,32 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator;
 
+/**
+ * An interface for unifying access to different types of validated data. A class implementing it called "data set".
+ */
 interface DataSetInterface
 {
     /**
-     * Get specified attribute value.
+     * Returns an attribute value by its name.
+     *
+     * @param string $attribute Attribute name.
+     * @return mixed Attribute value.
      */
     public function getAttributeValue(string $attribute): mixed;
 
+    /**
+     * Returns the validated data as a whole.
+     *
+     * @return mixed Validated data.
+     */
     public function getData(): mixed;
 
     /**
-     * If there is such attribute in the set.
+     * Whether a data set has the attribute with a given name. Note that this means existence only and attributes with
+     * empty values are treated as present.
+     *
+     * @param string $attribute Attribute name.
+     * @return bool Whether the attribute exists: `true` - exists and `false` - otherwise.
      */
     public function hasAttribute(string $attribute): bool;
 }
