f8c86cce2bfcf84e082195db2c83583a24a7cb85
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-12-11 11:47:03 +0300

    phpdoc

--- src/AttributeTranslatorProviderInterface.php
+++ src/AttributeTranslatorProviderInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator;
 
+/**
+ * Provides implementation of {@see AttributeTranslatorInterface} for translate attribute names.
+ */
 interface AttributeTranslatorProviderInterface
 {
     public function getAttributeTranslator(): ?AttributeTranslatorInterface;
