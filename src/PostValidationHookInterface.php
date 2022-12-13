f4b49f3a90776c57aae8b2436e67d63cb7268c8a
Alexey Rogachev <arogachev90@gmail.com>
2022-12-13 19:06:25 +0600

    Move see tag [skip ci]

--- src/PostValidationHookInterface.php
+++ src/PostValidationHookInterface.php
@@ -8,9 +8,6 @@ namespace Yiisoft\Validator;
  * An optional interface for data set ({@see DataSetInterface}) to implement (used as a replacement). It provides
  * {@see processValidationResult()} method-based hook allowing to execute custom code after a validation {@see Result}
  * has been formed.
- *
- * @see https://github.com/yiisoft/form/blob/4b385ff44ee1a5f402471e7a1e6aafff31a391fb/src/FormModel.php#L202 for usage
- * in `FormModel`.
  */
 interface PostValidationHookInterface extends DataSetInterface
 {
@@ -18,6 +15,9 @@ interface PostValidationHookInterface extends DataSetInterface
      * Method-based hook allowing to execute custom code after a validation {@see Result} has been formed.
      *
      * @param Result $result A validation {@see Result} instance.
+     *
+     * @see https://github.com/yiisoft/form/blob/4b385ff44ee1a5f402471e7a1e6aafff31a391fb/src/FormModel.php#L202 for
+     * usage in `FormModel`.
      */
     public function processValidationResult(Result $result): void;
 }
