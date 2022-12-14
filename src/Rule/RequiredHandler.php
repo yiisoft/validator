b87eb15e922d6401365fbafbbf5719cac8558437
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-12-14 10:16:14 +0300

    Allow set default empty criteria for `Required` rule

--- src/Rule/RequiredHandler.php
+++ src/Rule/RequiredHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Validator\Rule;
 
+use Yiisoft\Validator\EmptyCriteria\WhenEmpty;
 use Yiisoft\Validator\Exception\UnexpectedRuleException;
 use Yiisoft\Validator\Result;
 use Yiisoft\Validator\RuleHandlerInterface;
@@ -11,9 +12,26 @@ use Yiisoft\Validator\ValidationContext;
 
 /**
  * Validates that the specified value is passed and not empty.
+ *
+ * @psalm-import-type EmptyCriteriaType from Required
  */
 final class RequiredHandler implements RuleHandlerInterface
 {
+    /**
+     * @var callable
+     * @psalm-var EmptyCriteriaType
+     */
+    private $defaultEmptyCriteria;
+
+    /**
+     * @psalm-param EmptyCriteriaType|null $defaultEmptyCriteria
+     */
+    public function __construct(
+        callable|null $defaultEmptyCriteria = null,
+    ) {
+        $this->defaultEmptyCriteria = $defaultEmptyCriteria ?? new WhenEmpty(trimString: true);
+    }
+
     public function validate(mixed $value, object $rule, ValidationContext $context): Result
     {
         if (!$rule instanceof Required) {
@@ -27,7 +45,9 @@ final class RequiredHandler implements RuleHandlerInterface
             return $result;
         }
 
-        if (!$rule->getEmptyCriteria()($value, $context->isAttributeMissing())) {
+        $emptyCriteria = $rule->getEmptyCriteria() ?? $this->defaultEmptyCriteria;
+
+        if (!$emptyCriteria($value, $context->isAttributeMissing())) {
             return $result;
         }
 
