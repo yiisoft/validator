14a762793a6159ce985c0c0362c6e573dc61ffcf
Alexey Rogachev <arogachev90@gmail.com>
2022-12-20 14:56:40 +0600

    Fix tests

--- tests/ValidationContextTest.php
+++ tests/ValidationContextTest.php
@@ -62,7 +62,7 @@ final class ValidationContextTest extends TestCase
         $context = new ValidationContext();
 
         $this->expectException(RuntimeException::class);
-        $this->expectErrorMessage('Validator and raw data in validation context is not set.');
+        $this->expectErrorMessage('Validator in validation context is not set.');
         $context->validate(42);
     }
 
@@ -71,7 +71,7 @@ final class ValidationContextTest extends TestCase
         $context = new ValidationContext();
 
         $this->expectException(RuntimeException::class);
-        $this->expectErrorMessage('Validator and raw data in validation context is not set.');
+        $this->expectErrorMessage('Validator in validation context is not set.');
         $context->getRawData();
     }
 
