072cafbda03b27fb7f3600661dca1b974233b741
Alexey Rogachev <arogachev90@gmail.com>
2022-12-27 17:07:42 +0600

    Add docs to Json rule and its handler

--- src/Rule/Json.php
+++ src/Rule/Json.php
@@ -15,7 +15,10 @@ use Yiisoft\Validator\SkipOnErrorInterface;
 use Yiisoft\Validator\WhenInterface;
 
 /**
- * Validates that the value is a valid json.
+ * Contains a set of options to determine if the value is a valid JSON string.
+ *
+ * @see https://en.wikipedia.org/wiki/JSON
+ * @see https://tools.ietf.org/html/rfc8259
  *
  * @psalm-import-type WhenType from WhenInterface
  */
@@ -26,18 +29,23 @@ final class Json implements RuleWithOptionsInterface, SkipOnErrorInterface, When
     use SkipOnErrorTrait;
     use WhenTrait;
 
+    /**
+     * @param string $incorrectInputMessage Error message used when validation fails because the type of validated value
+     * is not a string.
+     * @param string $message Error message used when validation fails because the validated value either not JSON at
+     * all or invalid JSON with errors.
+     * @param bool|callable|null $skipOnEmpty Whether to skip this rule if the validated value is empty / not passed.
+     * See {@see SkipOnEmptyInterface}.
+     * @param bool $skipOnError Whether to skip this rule if any of the previous rules gave an error. See
+     * {@see SkipOnErrorInterface}.
+     * @param Closure|null $when A callable to define a condition for applying the rule. See {@see WhenInterface}.
+     * @psalm-param WhenType $when
+     */
     public function __construct(
         private string $incorrectInputMessage = 'The value must have a string type.',
         private string $message = 'The value is not JSON.',
-
-        /**
-         * @var bool|callable|null
-         */
-        private $skipOnEmpty = null,
+        private mixed $skipOnEmpty = null,
         private bool $skipOnError = false,
-        /**
-         * @var WhenType
-         */
         private Closure|null $when = null,
     ) {
     }
@@ -47,11 +55,26 @@ final class Json implements RuleWithOptionsInterface, SkipOnErrorInterface, When
         return 'json';
     }
 
+    /**
+     * Gets error message used when validation fails because the type of validated value is not a string.
+     *
+     * @return string Error message / template.
+     *
+     * @see $incorrectInputMessage
+     */
     public function getIncorrectInputMessage(): string
     {
         return $this->incorrectInputMessage;
     }
 
+    /**
+     * Error message used when validation fails because the validated value either not JSON at all or invalid JSON with
+     * errors.
+     *
+     * @return string Error message / template.
+     *
+     * @see $message
+     */
     public function getMessage(): string
     {
         return $this->message;
