ae55ce9eb48b6f5569291ec88956a09dc2177399
Alexey Rogachev <arogachev90@gmail.com>
2022-12-28 18:19:45 +0600

    Add docs for StopOnError and handler

--- src/Rule/StopOnErrorHandler.php
+++ src/Rule/StopOnErrorHandler.php
@@ -10,25 +10,8 @@ use Yiisoft\Validator\RuleHandlerInterface;
 use Yiisoft\Validator\ValidationContext;
 
 /**
- * Can be used for early stopping the validation process of the value when the validation on the stage was failed.
- *
- * For example, we have several rules, but we want not to process the rest rules when fail was ocurred:
- *
- * ```php
- * $request = [
- *     'username' => 'yiisoft',
- * ];
- * ```
- *
- * So to make validation we can configure it like this:
- *
- * ```php
- * $rule = new StopOnError([
- *      new HasLength(min: 3),
- *      // Heavy operation. It would be great not to call it if the previous rule was failed.
- *      new ExistsInDatabase(),
- * ]);
- * ```
+ * A handler for {@see StopOnError} rule. Validates a set of rules consecutively and stops at the rule where validation
+ * has failed.
  */
 final class StopOnErrorHandler implements RuleHandlerInterface
 {
