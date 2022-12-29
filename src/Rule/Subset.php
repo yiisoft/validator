1dbddbbf767ebaa93fe14350f2c670f5363cb0b5
Alexey Rogachev <arogachev90@gmail.com>
2022-12-29 14:01:34 +0600

    Fix linebreak [skip ci]

--- src/Rule/Subset.php
+++ src/Rule/Subset.php
@@ -20,8 +20,8 @@ use Yiisoft\Validator\WhenInterface;
  *
  * The order of items in the validated set is not important.
  *
- * Nested arrays are supported in both {@see $values} argument and in the validated value (the order of values in
- * lists must match, the order of keys in associative arrays is not important).
+ * Nested arrays are supported in both {@see $values} argument and in the validated value (the order of values in lists
+ * must match, the order of keys in associative arrays is not important).
  *
  * @see SubsetHandler Corresponding handler performing the actual validation.
  *
