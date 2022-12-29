618d6008e325a11f4d1d63a09333f5ef3e417ebd
Alexey Rogachev <arogachev90@gmail.com>
2022-12-29 13:25:26 +0600

    class implementing -> object implementing

--- src/Rule/Count.php
+++ src/Rule/Count.php
@@ -52,7 +52,7 @@ final class Count implements
      * {@see $exactly}. See {@see $greaterThanMaxMessage} for the customized message for a value with too many items.
      * @param int|null $exactly Exact number of items. `null` means no strict comparison. Mutually exclusive with
      * {@see $min} and {@see $max}.
-     * @param string $incorrectInputMessage Error message used when the value is neither an array nor a class
+     * @param string $incorrectInputMessage Error message used when the value is neither an array nor an object
      * implementing {@see \Countable} interface.
      *
      * You may use the following placeholders in the message:
