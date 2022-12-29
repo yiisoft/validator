5fca6791fc8b4a4564e7d66051e3529728b87485
Alexey Rogachev <arogachev90@gmail.com>
2022-12-29 18:15:29 +0600

    Sync with master

--- src/Rule/StopOnError.php
+++ src/Rule/StopOnError.php
@@ -14,10 +14,26 @@ use Yiisoft\Validator\RuleInterface;
 use Yiisoft\Validator\Helper\RulesDumper;
 use Yiisoft\Validator\RuleWithOptionsInterface;
 use Yiisoft\Validator\SkipOnEmptyInterface;
+use Yiisoft\Validator\SkipOnErrorInterface;
 use Yiisoft\Validator\WhenInterface;
 
 /**
- * Can be used for validation of nested structures.
+ * Applies to a set of rules, runs validation for each one of them in the order they are defined and stops at the rule
+ * where validation failed. In particular, it can be useful for preventing the heavy operations to increase performance.
+ * It can be set like that for a group of ordered rules:
+ *
+ * ```php
+ * $rule = new StopOnError([
+ *      new HasLength(min: 3),
+ *      // This operation executes DB query and thus heavier. It's preferable not to call it if the previous rule did
+ *      not pass the validation.
+ *      new ExistsInDatabase(),
+ * ]);
+ * ```
+ *
+ * Not to be confused with skipping, there is a separate functionality for that, see {@see SkipOnErrorInterface}.
+ *
+ * @see StopOnErrorHandler Corresponding handler performing the actual validation.
  *
  * @psalm-import-type WhenType from WhenInterface
  */
@@ -31,20 +47,25 @@ final class StopOnError implements
     use SkipOnEmptyTrait;
     use WhenTrait;
 
+    /**
+     * @var RulesDumper|null A rules dumper instance used to dump {@see $rules} as array. Lazily created by
+     * {@see getRulesDumper()} only when it's needed.
+     */
     private ?RulesDumper $rulesDumper = null;
 
+    /**
+     * @param iterable $rules A set of rules for running the validation. Note that they are not normalized.
+     * @psalm-param iterable<RuleInterface> $rules
+     *
+     * @param bool|callable|null $skipOnEmpty Whether to skip this `StopOnError` rule with all defined {@see $rules} if
+     * the validated value is empty / not passed. See {@see SkipOnEmptyInterface}.
+     * @param Closure|null $when A callable to define a condition for applying this `StopOnError` rule with all defined
+     * {@see $rules}. See {@see WhenInterface}.
+     * @psalm-param WhenType $when
+     */
     public function __construct(
-        /**
-         * @var iterable<RuleInterface>
-         */
         private iterable $rules,
-        /**
-         * @var bool|callable|null
-         */
         private mixed $skipOnEmpty = null,
-        /**
-         * @var WhenType
-         */
         private Closure|null $when = null,
     ) {
     }
@@ -55,7 +76,11 @@ final class StopOnError implements
     }
 
     /**
-     * @return iterable<RuleInterface>
+     * Gets a set of rules for running the validation.
+     *
+     * @return iterable A set of rules.
+     *
+     * @psalm-return iterable<RuleInterface>
      */
     public function getRules(): iterable
     {
@@ -70,7 +95,7 @@ final class StopOnError implements
     {
         return [
             'skipOnEmpty' => $this->getSkipOnEmptyOption(),
-            'rules' => $this->getRulesDumper()->asArray($this->rules),
+            'rules' => $this->dumpRulesAsArray(),
         ];
     }
 
@@ -88,6 +113,24 @@ final class StopOnError implements
         }
     }
 
+    /**
+     * Dumps defined {@see $rules} to array.
+     *
+     * @return array The array of rules with their options.
+     */
+    private function dumpRulesAsArray(): array
+    {
+        return $this->getRulesDumper()->asArray($this->getRules());
+    }
+
+    /**
+     * Returns existing rules dumper instance for dumping defined {@see $rules} as array if it's already set. If not set
+     * yet, creates the new instance first.
+     *
+     * @return RulesDumper A rules dumper instance.
+     *
+     * @see $rulesDumper
+     */
     private function getRulesDumper(): RulesDumper
     {
         if ($this->rulesDumper === null) {
