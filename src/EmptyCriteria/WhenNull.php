551582b1a18dc60dbd184aa937be8e0a35a2e4d9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-01-07 22:47:20 +0300

    Improve description of `WhenNull` + more tests (#499)

--- src/EmptyCriteria/WhenNull.php
+++ src/EmptyCriteria/WhenNull.php
@@ -7,8 +7,8 @@ namespace Yiisoft\Validator\EmptyCriteria;
 /**
  * Empty criteria is a callable returning `true` if a value must be considered empty.
  *
- * With `WhenNull` a rule is considered empty only when the value is `null`. With regard to validation process, a
- * corresponding rule is skipped only if this condition is met and `WhenNull` is set:
+ * With `WhenNull` a rule is considered empty only when the value is `null` or the attribute is missing. With regard
+ * to validation process, a corresponding rule is skipped only if this condition is met and `WhenNull` is set:
  *
  * - At a rule level via `$skipOnEmpty` property, but only for rules implementing {@see SkipOnEmptyTrait} / including
  * {@see SkipOnEmptyTrait}.
