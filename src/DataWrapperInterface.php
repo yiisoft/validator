dd9bb58369f0966e8a7b51e388637d5768eda2d5
Alexander Makarov <sam@rmcreative.ru>
2023-01-08 10:56:11 +0400

    Add DataWrapperInterface docs

--- src/DataWrapperInterface.php
+++ src/DataWrapperInterface.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 namespace Yiisoft\Validator;
 
 /**
+ * Data wrapper interface provides access to raw data behind a data set.
+ *
  * @internal
  */
 interface DataWrapperInterface extends DataSetInterface
 {
+    /**
+     * Get raw data that is wrapped.
+     *
+     * @return mixed Raw data.
+     */
     public function getSource(): mixed;
 }
