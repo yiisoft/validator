bbc6702bf95f31de4478b15a00f998c030b96ffe
Alexey Rogachev <arogachev90@gmail.com>
2023-01-09 18:06:50 +0600

    Add docs to Nested rule and handler

--- src/Validator.php
+++ src/Validator.php
@@ -29,13 +29,13 @@ use function is_string;
 final class Validator implements ValidatorInterface
 {
     /**
-     * @const A name for {@see CategorySource} used with translator ({@see TranslatorInterface}) by default.
+     * A name for {@see CategorySource} used with translator ({@see TranslatorInterface}) by default.
      */
     public const DEFAULT_TRANSLATION_CATEGORY = 'yii-validator';
     /**
-     * @const A name of parameter used in {@see ValidationContext} indicating that previous rule in the set caused
-     * validation error. Used with {@see SkipOnErrorInterface} to allow skipping of the current rule if its
-     * configuration allows it.
+     * A name of parameter used in {@see ValidationContext} indicating that previous rule in the set caused validation
+     * error. Used with {@see SkipOnErrorInterface} to allow skipping of the current rule if its configuration allows
+     * it.
      */
     private const PARAMETER_PREVIOUS_RULES_ERRORED = 'yii-validator-previousRulesErrored';
 
