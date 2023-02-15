1cfbcccbf8213ce551558a0c4c62f908ceae661c
Alexey Rogachev <arogachev90@gmail.com>
2023-02-15 13:43:13 +0600

    More specific data set name

--- tests/Rule/NestedTest.php
+++ tests/Rule/NestedTest.php
@@ -1073,7 +1073,7 @@ final class NestedTest extends RuleTestCase
                     'attributes.abc' => ['Value cannot be blank.'],
                 ],
             ],
-            'deep level' => [
+            'deep level of nesting with bare keys' => [
                 [
                     'level1' => [
                         'level2' => [
