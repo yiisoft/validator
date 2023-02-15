3ead456a32c9e3f2ce89eafc802d7f2da74e6a55
Alexey Rogachev <arogachev90@gmail.com>
2023-02-15 13:42:22 +0600

    Revert commit

--- tests/Rule/NestedTest.php
+++ tests/Rule/NestedTest.php
@@ -1073,7 +1073,7 @@ final class NestedTest extends RuleTestCase
                     'attributes.abc' => ['Value cannot be blank.'],
                 ],
             ],
-            'deep level of nesting with bare keys' => [
+            'deep level' => [
                 [
                     'level1' => [
                         'level2' => [
