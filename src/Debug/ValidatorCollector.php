90694823a534daa3c04b9d51b132b1b5ebabd5bd
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-06-02 10:43:44 +0300

    Fix summary in validator collector for debug

--- src/Debug/ValidatorCollector.php
+++ src/Debug/ValidatorCollector.php
@@ -42,6 +42,10 @@ final class ValidatorCollector implements SummaryCollectorInterface
     public function getSummary(): array
     {
         $count = count($this->validations);
+        if ($count === 0) {
+            return [];
+        }
+
         $countValid = count(array_filter($this->validations, fn (array $data): bool => (bool)$data['result']));
         $countInvalid = $count - $countValid;
 
