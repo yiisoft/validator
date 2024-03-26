4cea850c8331299f7ad7d67153eab88ec8c7ba57
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-03-26 15:39:58 +0300

    add tests

---
+++ tests/Rule/Image/StubImageInfoProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Tests\Rule\Image;
+
+use Yiisoft\Validator\Rule\Image\ImageInfo;
+use Yiisoft\Validator\Rule\Image\ImageInfoProviderInterface;
+
+final class StubImageInfoProvider implements ImageInfoProviderInterface
+{
+    public function __construct(
+        private ?ImageInfo $info = null,
+    ) {
+    }
+
+    public function get(string $path): ?ImageInfo
+    {
+        return $this->info;
+    }
+}
