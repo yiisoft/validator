9e4bb6dac047fcd7e03cdf347ccda86b5f8a9711
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-02-06 17:54:18 +0300

    fix

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 
@@ -21,4 +22,7 @@ return RectorConfig::configure()
         ClosureToArrowFunctionRector::class,
         NullToStrictStringFuncCallArgRector::class,
         NewInInitializerRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class => [
+            __DIR__ . '/src/Rule/Composite.php',
+        ]
     ]);
