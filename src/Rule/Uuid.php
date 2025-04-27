a412e74776f9abe58f57f79d0fca70c459442c9c
Alexandr Sakhnov <sakhnov.krg@gmail.com>
2025-04-27 15:35:29 +0500

    Add UUID validator

---
+++ src/Rule/Uuid.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Rule;
+
+use Attribute;
+use Closure;
+use InvalidArgumentException;
+use Yiisoft\Validator\DumpedRuleInterface;
+use Yiisoft\Validator\Rule\Trait\SkipOnEmptyTrait;
+use Yiisoft\Validator\Rule\Trait\SkipOnErrorTrait;
+use Yiisoft\Validator\Rule\Trait\WhenTrait;
+use Yiisoft\Validator\SkipOnEmptyInterface;
+use Yiisoft\Validator\SkipOnErrorInterface;
+use Yiisoft\Validator\WhenInterface;
+
+/**
+ * Checks if the value is a valid UUID.
+ *
+ * @psalm-import-type SkipOnEmptyValue from SkipOnEmptyInterface
+ * @psalm-import-type WhenType from WhenInterface
+ *
+ * @see UuidHandler
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
+final class Uuid implements DumpedRuleInterface, SkipOnEmptyInterface, SkipOnErrorInterface, WhenInterface
+{
+    use SkipOnEmptyTrait;
+    use SkipOnErrorTrait;
+    use WhenTrait;
+
+    /**
+     * @param string $incorrectInputMessage A message used when the input is incorrect.
+     * @param string $message Error message used when validation fails due to the wrong UUID format.
+     * @param bool|callable|null $skipOnEmpty Whether to skip this rule if the validated value is empty / not passed.
+     * See {@see SkipOnEmptyInterface}.
+     * @param bool $skipOnError Whether to skip this rule if any of the previous rules gave an error. See
+     * {@see SkipOnErrorInterface}.
+     * @param Closure|null $when A callable to define a condition for applying the rule. See {@see WhenInterface}.
+     *
+     * @psalm-param SkipOnEmptyValue $skipOnEmpty
+     * @psalm-param WhenType $when
+     *
+     * @throws InvalidArgumentException If configuration is wrong.
+     */
+    public function __construct(
+        private string $incorrectInputMessage = '{Property} must be a string. {type} given.',
+        private string $message = 'The value of {property} is not a valid UUID.',
+        bool|callable|null $skipOnEmpty = null,
+        private bool $skipOnError = false,
+        private Closure|null $when = null,
+    ) {
+        $this->skipOnEmpty = $skipOnEmpty;
+    }
+
+    public function getName(): string
+    {
+        return self::class;
+    }
+
+    /**
+     * Get a message used when the input it incorrect.
+     *
+     * @return string Error message
+     *
+     * @see $incorrectInputMessage
+     */
+    public function getIncorrectInputMessage(): string
+    {
+        return $this->incorrectInputMessage;
+    }
+
+    /**
+     * Get an error message used when validation fails due to the wrong UUID format.
+     *
+     * @return string Error message.
+     *
+     * @see $message
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function getOptions(): array
+    {
+        return [
+            'incorrectInputMessage' => [
+                'template' => $this->incorrectInputMessage,
+                'parameters' => [],
+            ],
+            'message' => [
+                'template' => $this->message,
+                'parameters' => [],
+            ],
+            'skipOnEmpty' => $this->getSkipOnEmptyOption(),
+            'skipOnError' => $this->skipOnError,
+        ];
+    }
+
+    public function getHandler(): string
+    {
+        return UuidHandler::class;
+    }
+}
