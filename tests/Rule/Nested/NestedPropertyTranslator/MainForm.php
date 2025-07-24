f3d0638d86a93e8e4cc739e2f040c51ab78d62ef
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-07-24 23:15:59 +0300

    test

---
+++ tests/Rule/Nested/NestedPropertyTranslator/MainForm.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Validator\Tests\Rule\Nested\NestedPropertyTranslator;
+
+use Yiisoft\Validator\Rule\Nested;
+
+final class MainForm {
+    public function __construct(
+        #[Nested]
+        public SubForm $sub,
+    ) {
+    }
+}
